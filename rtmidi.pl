6d967bf590aafd79e733ffb737f4ea4efaaaee1b
Gene Boggs <gene@ology.net>
2024-05-15 19:06:09 -0700

    Micro- not millis!

--- rtmidi.pl
+++ rtmidi.pl
@@ -11,7 +11,7 @@ use warnings;
 #use lib map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Util); # n.b. local author libs. comment this out unless you're me
 use Getopt::Long qw(GetOptions);
 use MIDI::RtMidi::FFI::Device ();
-use MIDI::Util qw(setup_score midi_format score2events get_milliseconds);
+use MIDI::Util qw(setup_score midi_format score2events get_microseconds);
 use Time::HiRes qw(usleep);
 
 my %opt = (
@@ -39,7 +39,7 @@ for my $note (@notes) {
     $score->n($duration, midi_format($pitch));
 }
 
-my $millis = get_milliseconds($score);
+my $micros = get_microseconds($score);
 
 my $events = score2events($score);
 
@@ -53,7 +53,7 @@ sleep 1;
 for my $event (@$events) {
     my $name = $event->[0];
     if ($name =~ /^note_\w+$/) {
-        my $useconds = $millis * $event->[1];
+        my $useconds = $micros * $event->[1];
         usleep($useconds) if $name eq 'note_off';
         $device->send_event($name => @{ $event }[ 2 .. 4 ]);
     }
