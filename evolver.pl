3143259a5dc913cc34f1c082519f8f3140465528
Gene Boggs <gene@ology.net>
2024-07-03 19:10:21 -0700

    WIP

--- evolver.pl
+++ evolver.pl
@@ -7,12 +7,12 @@ use if $ENV{USER} eq 'gene', lib => map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-
 use AI::Genetic ();
 use Data::Dumper::Compact qw(ddc);
 use Getopt::Long qw(GetOptions);
-use List::Util qw(pairs sum0);
+use List::Util qw(pairs);
 use MIDI::Util qw(dura_size);
 
 my %opt = (
     type       => 'listvector',
-    population => 4,
+    population => 500,
     crossover  => 0.9,  # probability of crossover
     mutation   => 0.01, # probability of mutation
 );
@@ -25,14 +25,12 @@ GetOptions(\%opt,
 sub fitness {
     my ($chromosome) = @_;
 # warn __PACKAGE__,' L',__LINE__,' ',ddc($chromosome, {max_width=>128});
-    my @durations;
+    my $sum = 0;
     for my $pair (pairs @$chromosome) {
         next if $pair->[0] eq '0' || $pair->[1] eq '0';
-        push @durations, dura_size($pair->[0]);
+        $sum += dura_size($pair->[0]);
     }
-warn __PACKAGE__,' L',__LINE__,' ',ddc(\@durations, {max_width=>128});
-    my $sum = sum0(@durations);
-warn __PACKAGE__,' L',__LINE__,' ',,"S: $sum\n";
+# warn __PACKAGE__,' L',__LINE__,' ',,"S: $sum\n";
     return $sum;
 }
 
@@ -59,9 +57,9 @@ $ga->init([
 ]);
 # warn __PACKAGE__,' L',__LINE__,' ',ddc($ga->people);
 
-$ga->evolve('rouletteTwoPoint', 1);
+$ga->evolve('rouletteTwoPoint', 100);
 # warn __PACKAGE__,' L',__LINE__,' ',ddc($ga->people);
 
-# print 'Fittest: ', ddc($ga->getFittest, {max_width=>128});
+print 'Fittest: ', ddc($ga->getFittest, {max_width=>128});
 
 __END__
