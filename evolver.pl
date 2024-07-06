b7078c9af1acf82c371947c183be94fa355bd94a
Gene Boggs <gene@ology.net>
2024-07-06 01:05:09 -0700

    WIP

--- evolver.pl
+++ evolver.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use if $ENV{USER} eq 'gene', lib => map { "$ENV{HOME}/sandbox/$_/lib" } qw(MIDI-Util);
 
+use Algorithm::Combinatorics qw(permutations);
 use Data::Dumper::Compact qw(ddc);
 use Getopt::Long qw(GetOptions);
-use List::Util qw(all min);
+use Integer::Partition ();
+use List::Util qw(all min uniq);
+use MIDI::Util qw(dura_size reverse_dump);
 
 my %opt = (
     mother  => 'qn hn hn qn hn',
@@ -18,10 +22,55 @@ GetOptions(\%opt,
     'verbose!',
 );
 
-my %rules = (
+my $factor = 2;
+
+my %rules;
+my %seen;
+
+for my $dura (qw(dhn hn)) {
+    my $ip = Integer::Partition->new(dura_size($dura) * $factor);
+    my @parts;
+    while (my $p = $ip->next) {
+        next if @$p <= 1;
+        if (uniq(@$p) > 1) {
+            my $iter = permutations($p);
+            while (my $perm = $iter->next) {
+                push @parts, $perm unless $seen{"@$perm"}++;
+            }
+        }
+        else {
+            push @parts, $p unless $seen{"@$p"}++;
+        }
+    }
+warn __PACKAGE__,' L',__LINE__,' ',ddc(\@parts);
+    # print "$dura: ",ddc(\@parts);
+    my $rev = reverse_dump('length');
+    # $rules{$dura} = [ map { join(' ', map { $rev->{ $_ / $factor } } @$_) } @parts ];
+    my @z;
+    for my $x (@parts) {
+        my @temp;
+        for (@$x) {
+            my $f = $_ / $factor;
+            my $y = $rev->{$f};
+            push @temp, $y;
+        }
+        next if grep { !defined } @temp;
+        push @z, join ' ', @temp;
+    }
+    $rules{$dura} = \@z;
+}
+warn __PACKAGE__,' L',__LINE__,' ',ddc(\%rules);
+exit;
+
+my %rulesX = (
+    dhn => [
+      'hn qn',
+      'qn hn',
+      'qn qn qn',
+    ],
     hn => [
-      'den en',
-      'en den',
+      'dqn qn',
+      'qn dqn',
       'qn qn',
       'qn en en',
       'en qn en',
@@ -41,6 +90,43 @@ my $father = [ split /\s+/, $opt{father} ];
 print '1st mother: ',ddc($mother);
 print '1st father: ',ddc($father);
 
+my @mother_dura = map { dura_size($_) } @$mother;
+warn __PACKAGE__,' L',__LINE__,' ',ddc(\@mother_dura);
+my @father_dura = map { dura_size($_) } @$father;
+warn __PACKAGE__,' L',__LINE__,' ',ddc(\@father_dura);
+my $x = int rand 8;
+warn __PACKAGE__,' L',__LINE__,' ',,"X: $x\n";
+my $sum = 0;
+my $i = 0;
+for my $n (@mother_dura) {
+    $sum += $n;
+    if ($x <= $sum) {
+        if ($n == 2) {
+            splice @$mother, $i, 1, qw(qn qn);
+            $i++ if $x == $sum;
+        }
+        warn "$i: $sum, $n\n";
+        last;
+    }
+    $i++;
+}
+warn __PACKAGE__,' L',__LINE__,' M: ',ddc($mother);
+$sum = 0;
+my $j = 0;
+for my $n (@father_dura) {
+    $sum += $n;
+    if ($x <= $sum) {
+        if ($n == 2) {
+            splice @$father, $j, 1, qw(qn qn);
+            $j++ if $x == $sum;
+        }
+        warn "$j: $sum, $n\n";
+        last;
+    }
+    $j++;
+}
+warn __PACKAGE__,' L',__LINE__,' F: ',ddc($father);
+
 # my $matches = subsequences($mother, $father);
 # warn 'Matches: ',ddc($matches) if $opt{verbose};
 
@@ -49,9 +135,7 @@ print '1st father: ',ddc($father);
 # $child = mutate_up(\%inverted, $child, $opt{mutate});
 # print '3rd: ',ddc($child);
 
-my @mother_list = (1, 2, 3, 4, 5);
-my @father_list = (6, 7, 8, 9, 10);
-my ($child_mother, $child_father) = crossover($mother, $father);
+my ($child_mother, $child_father) = crossover($mother, $father, $i, $j);
 print "Mother's child: ", ddc($child_mother);
 print "Father's child: ", ddc($child_father);
 
@@ -137,17 +221,30 @@ sub subsequences {
 }
 
 sub crossover {
-    my ($mother, $father) = @_;
+    my ($mother, $father, $m_point, $f_point) = @_;
+warn __PACKAGE__,' L',__LINE__,' ',,"P: $m_point, $f_point\n";
     my @mother = @$mother;
     my @father = @$father;
     my $mother_size = @mother;
     my $father_size = @father;
     my $min_size = min($mother_size, $father_size);
-    my $point = int rand $min_size;
-warn __PACKAGE__,' L',__LINE__,' ',,"P: $point\n";
     # Swap elements beyond the crossover point
-    for my $i ($point .. $min_size - 1) {
-        ($mother[$i], $father[$i]) = ($father[$i], $mother[$i]);
+    while ($m_point < $mother_size && $f_point < $father_size) {
+        if ($mother[$m_point] eq $father[$f_point]) {
+            $m_point++;
+            $f_point++;
+            next;
+        }
+        if ($mother[$m_point] eq 'hn' && $father[$f_point] eq 'qn' && $father[$f_point + 1] eq 'qn') {
+            splice @mother, $m_point, 1, qw(qn qn);
+            splice @father, $f_point, 2, qw(hn);
+        }
+        elsif ($father[$f_point] eq 'hn' && $mother[$m_point] eq 'qn' && $mother[$m_point + 1] eq 'qn') {
+            splice @father, $f_point, 1, qw(qn qn);
+            splice @mother, $m_point, 2, qw(hn);
+        }
+        $m_point++;
+        $f_point++;
     }
     @mother = grep { defined } @mother;
     @father = grep { defined } @father;
