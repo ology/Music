24fbd19051899d93b0c01401432a2cd994a363f1
Gene Boggs <gene@ology.net>
2024-07-07 00:45:26 -0700

    Comment

--- evolver.pl
+++ evolver.pl
@@ -63,7 +63,7 @@ sub substitution {
     # compute the father iterator and division
     (my $j, $sum) = iter($x, $father_dura);
     my $f_div = $sum - $x;
-    warn __PACKAGE__,' L',__LINE__,' ',,"M/F divs: $m_div, $f_div\n";
+    # warn __PACKAGE__,' L',__LINE__,' ',,"M/F divs: $m_div, $f_div\n";
     my ($m_incd, $f_incd) = (0, 0);
     if (($m_div <= 0) || ($i != $j && $mother_dura->[$i] != $father_dura->[$j])) {
         $m_div++;
@@ -75,12 +75,12 @@ sub substitution {
     }
     my $m_size = $mother_dura->[$i] - $m_div;
     my $f_size = $father_dura->[$j] - $f_div;
-    warn __PACKAGE__,' L',__LINE__,' ',,"Msize: $mother_dura->[$i] - $m_div = $m_size\n";
-    warn __PACKAGE__,' L',__LINE__,' ',,"Fsize: $father_dura->[$j] - $f_div = $f_size\n";
+    # warn __PACKAGE__,' L',__LINE__,' ',,"Msize: $mother_dura->[$i] - $m_div = $m_size\n";
+    # warn __PACKAGE__,' L',__LINE__,' ',,"Fsize: $father_dura->[$j] - $f_div = $f_size\n";
     my $m_sub = gen_sub($m_div, $m_size, $mother, $mother_dura, $i, $m_incd);
-    warn __PACKAGE__,' L',__LINE__,' ',,"Msub: @$m_sub\n";
+    # warn __PACKAGE__,' L',__LINE__,' ',,"Msub: @$m_sub\n";
     my $f_sub = gen_sub($f_div, $f_size, $father, $father_dura, $j, $f_incd);
-    warn __PACKAGE__,' L',__LINE__,' ',,"Fsub: @$f_sub\n";
+    # warn __PACKAGE__,' L',__LINE__,' ',,"Fsub: @$f_sub\n";
     # substitution
     splice @$mother, $i, 1, @$m_sub;
     splice @$father, $j, 1, @$f_sub;
