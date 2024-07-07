29475bda773254f3f82f7ce93402622749db84e0
Gene Boggs <gene@ology.net>
2024-07-06 23:54:05 -0700

    Random

--- evolver.pl
+++ evolver.pl
@@ -31,7 +31,7 @@ my ($rules, $inverted) = build_rules([qw(wn dhn hn qn)]);
 
 my ($mother, $father) = get_parents($opt{mother}, $opt{father});
 
-my $crossover = 6;#int(rand 8) + 1;
+my $crossover = int(rand 8) + 1;
 warn "Beat crossover point: $crossover\n" if $opt{verbose};
 
 my ($m_point, $f_point) = substitution($mother, $father, $crossover);
