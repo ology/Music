0d8be933d37bf12a09e53eb80a9273ec1be52a1c
Gene Boggs <gene@ology.net>
2024-12-01 20:22:30 -0800

    Comment

--- modal-stacked-3rds.pl
+++ modal-stacked-3rds.pl
@@ -17,10 +17,10 @@ for my $mode (qw(ionian dorian phrygian lydian mixolydian aeolian locrian)) {
 }
 
 __END__
-ionian:  C E G B D F A      = Cmaj13
-dorian:  C Eb G Bb D F A    = Cm13
-phryg:   C Eb G Bb Db F Ab  = Cm(b9,b13)
-lydian:  C E G B D F# A     = Cmaj13#11
-mixo:    C E G Bb D F A     = C13
-aeolian: C Eb G Bb D F Ab   = Cmb13
-loc:     C Eb Gb Bb Db F Ab = Cm(b5,b9,b13)
+ionian:  C E  G  B  D  F  A  = Cmaj13
+dorian:  C Eb G  Bb D  F  A  = Cm13
+phryg:   C Eb G  Bb Db F  Ab = Cm(b9,b13)
+lydian:  C E  G  B  D  F# A  = Cmaj13#11
+mixo:    C E  G  Bb D  F  A  = C13
+aeolian: C Eb G  Bb D  F  Ab = Cmb13
+loc:     C Eb Gb Bb Db F  Ab = Cm(b5,b9,b13)
