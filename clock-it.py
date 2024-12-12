67cd0201a9591769375b9794a672fad8a6e594a8
Gene Boggs <gene@ology.net>
2024-12-12 15:20:53 -0800

    Cruft

--- clock-it.py
+++ clock-it.py
@@ -13,16 +13,7 @@ def send_clock(outport, bpm=120):
         outport.close()
         print("\nExiting...")
 
-def send_clock_messages(outport, bpm=120, num_beats=4):
-    # Calculate the time between clock messages (24 PPQN per beat)
-    interval = 60 / (bpm * 24)
-    for _ in range(num_beats * 24):
-        outport.send(mido.Message('clock'))
-        time.sleep(interval)
-
 if __name__ == "__main__":
     with mido.open_output('USB MIDI Interface') as outport:
         outport.send(mido.Message('start'))
-        # send_clock_messages(outport, bpm=100, num_beats=8)
-        # outport.send(mido.Message('stop'))
         send_clock(outport, bpm=100)
