436649d1796be6111a594f8c3fb0dd74916fa3fa
Gene Boggs <gene@ology.net>
2025-02-13 19:58:28 -0800

    Broke WIP

--- applause.pl
+++ applause.pl
@@ -9,30 +9,36 @@ my $y = shift || 4;
 
 my $d = MIDI::Drummer::Tiny->new;
 
-my @rows = map { row($_) } 1 .. $y;
+my @rows = map { row($_)->@* } 1 .. $y;
 
 $d->sync(@rows);
+use Data::Dumper::Compact qw(ddc);
+warn __PACKAGE__,' L',__LINE__,' ',ddc([$d->score->Score], {max_width=>128});
 
 $d->play_with_timidity;
 # $d->write;
 
 sub row {
     my ($n) = @_;
-    my $sub = sub {
-        my $bpm = random_item([70 .. 100]);
-        my $reverb = 15 + $n;
-        my $volume = 100 - $n;
-        my $mdt = MIDI::Drummer::Tiny->new(
-            score  => $d->score,
-            bpm    => $bpm,
-            reverb => $reverb,
-            volume => $volume,
-        );
-        for my $i (1 .. $x) {
-            $mdt->count_in({ bars => 4, patch => $d->snare });
-        }
-    };
-    return $sub;
+    my @subs;
+    for (1 .. $x) {
+        push @subs, sub {
+            my $bpm = random_item([70 .. 100]);
+            my $reverb = 15 + $n;
+            my $volume = 100 - $n;
+            my $mdt = MIDI::Drummer::Tiny->new(
+                score  => $d->score,
+                setup  => 0,
+                bpm    => $bpm,
+                reverb => $reverb,
+                volume => $volume,
+            );
+            for (1 .. $mdt->beats) {
+                $mdt->note($mdt->quarter, $mdt->snare);
+            }
+        };
+    }
+    return \@subs;
 }
 
 sub random_item {
