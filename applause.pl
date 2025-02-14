b6ef08f26f7391c2b6ec4640c1d8167f61213eaa
Gene Boggs <gene@ology.net>
2025-02-13 19:08:54 -0800

    WIP

--- applause.pl
+++ applause.pl
@@ -7,9 +7,9 @@ use MIDI::Drummer::Tiny ();
 my $x = shift || 4;
 my $y = shift || 4;
 
-my @rows = map { row($_) } 1 .. $y;
-
 my $d = MIDI::Drummer::Tiny->new;
+
+my @rows = map { row($_) } 1 .. $y;
 
 $d->sync(@rows);
 
@@ -23,6 +23,7 @@ sub row {
         my $reverb = 15 + $n;
         my $volume = 100 - $n;
         my $mdt = MIDI::Drummer::Tiny->new(
+            score  => $d->score,
             bpm    => $bpm,
             reverb => $reverb,
             volume => $volume,
