1c4e6548e14833d22bfe4d53668e63f5b16c1914
Gene Boggs <gene@ology.net>
2025-02-18 20:53:47 -0800

    WIP

--- rtmidi-control.pl
+++ rtmidi-control.pl
@@ -5,11 +5,20 @@ use warnings;
 use Data::Dumper::Compact qw(ddc);
 use MIDI::RtMidi::FFI::Device ();
 
-my $midi_in = RtMidiIn->new;
-
+my $midi_in  = RtMidiIn->new;
 $midi_in->open_port_by_name(qr/tempopad/i);
+
+my $midi_out = RtMidiOut->new;
+$midi_out->open_virtual_port('foo');
+$midi_out->open_port_by_name(qr/fluid/i);
 
 while (1) {
     my $msg = $midi_in->get_message_decoded;
-    warn ddc($msg) if $msg;
+    if ($msg) {
+        warn ddc($msg);
+        $midi_out->note_on(0x00, 0x3C, 0x7A);
+        sleep 1;
+        $midi_out->note_off(0x00, 0x3C);
+        sleep 1;
+    }
 }
