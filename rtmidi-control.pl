0b86bc92d700990be03cf07e7dd6978f43840157
Gene Boggs <gene@ology.net>
2025-02-19 01:47:52 -0800

    Underscore

--- rtmidi-control.pl
+++ rtmidi-control.pl
@@ -18,37 +18,37 @@ while (1) {
     if ($msg && $msg->[0] eq 'note_on') {
         warn ddc($msg);
         if ($msg->[2] == 55) {
-            my $sleep = 500000;
+            my $sleep = 500_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 1);
         }
         elsif ($msg->[2] == 54 || $msg->[2] == 59) {
-            my $sleep = 300000;
+            my $sleep = 300_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 2);
         }
         elsif ($msg->[2] == 53 || $msg->[2] == 58 || $msg->[2] == 63) {
-            my $sleep = 100000;
+            my $sleep = 100_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 3);
         }
         elsif ($msg->[2] == 52 || $msg->[2] == 57 || $msg->[2] == 62 || $msg->[2] == 67) {
-            my $sleep = 50000;
+            my $sleep = 50_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 4);
         }
         elsif ($msg->[2] == 56 || $msg->[2] == 61 || $msg->[2] == 66) {
-            my $sleep = 40000;
+            my $sleep = 40_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 5);
         }
         elsif ($msg->[2] == 60 || $msg->[2] == 65) {
-            my $sleep = 30000;
+            my $sleep = 30_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 6);
         }
         elsif ($msg->[2] == 64) {
-            my $sleep = 20000;
+            my $sleep = 20_000;
             my $pitch = 60;
             delay_effect($midi_out, $msg, $pitch, $sleep, 7);
         }
