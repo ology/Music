03f6d918f72b98e416736ae36041b858e0b4bf21
Gene Boggs <gene@ology.net>
2025-02-18 21:18:20 -0800

    WIP

--- rtmidi-control.pl
+++ rtmidi-control.pl
@@ -16,7 +16,9 @@ while (1) {
     my $msg = $midi_in->get_message_decoded;
     if ($msg && $msg->[0] eq 'note_on') {
         warn ddc($msg);
-        $midi_out->note_on(0x00, 0x3C, 0x7A);
-        $midi_out->note_off(0x00, 0x3C);
+        my @spec = map { sprintf('0x%X', $_) } @$msg[1 .. 3];
+        $midi_out->note_on(@spec);
+        sleep 1;
+        $midi_out->note_off(@spec[1,2]);
     }
 }
