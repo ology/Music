b34b8e6cac6534fef839e375d1d5db573e1dfaf0
Gene Boggs <gene@ology.net>
2025-02-19 01:16:24 -0800

    WIP

--- rtmidi-control.pl
+++ rtmidi-control.pl
@@ -19,55 +19,29 @@ while (1) {
         warn ddc($msg);
         if ($msg->[2] == 55) {
             my $sleep = 500000;
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
+            my $pitch = 60;
+            single_note($midi_out, $msg, $pitch, $sleep);
         }
         elsif ($msg->[2] == 54 || $msg->[2] == 59) {
             my $sleep = 300000;
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
+            my $pitch = 60;
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
         }
         elsif ($msg->[2] == 53 || $msg->[2] == 58 || $msg->[2] == 63) {
             my $sleep = 100000;
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
+            my $pitch = 60;
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
         }
         elsif ($msg->[2] == 52 || $msg->[2] == 57 || $msg->[2] == 62 || $msg->[2] == 67) {
             my $sleep = 50000;
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
-            $midi_out->note_on($msg->[1], 60, $msg->[3]);
-            usleep($sleep);
-            $midi_out->note_off(@$msg[1], 60);
-            usleep($sleep);
+            my $pitch = 60;
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
+            single_note($midi_out, $msg, $pitch, $sleep);
         }
         elsif ($msg->[2] == 84) {
             $midi_out->note_on($msg->[1], 64, $msg->[3]);
@@ -78,6 +52,21 @@ while (1) {
         elsif ($msg->[2] == 51) {
             $midi_out->note_on($msg->[1], 71, $msg->[3]);
         }
+    }
+}
+
+sub single_note {
+    my ($out, $message, $note, $t) = @_;
+    $out->note_on($message->[1], $note, $message->[3]);
+    usleep($t);
+    $out->note_off(@$message[1], $note);
+    usleep($t);
+}
+
+sub delay_effect {
+    my ($out, $message, $note, $t, $feedback) = @_;
+    for my $f (1 .. $feedback) {
+        single_note($out, $message, $note, $t);
     }
 }
 
