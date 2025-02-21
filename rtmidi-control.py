eb64bd15034f8a501abae9403512c8f7f272f627
Gene Boggs <gene@ology.net>
2025-02-21 09:56:24 -0800

    New

---
+++ rtmidi-control.py
@@ -0,0 +1,29 @@
+import asyncio
+import mido
+import sys
+import os
+import time
+import rtmidi
+
+async def main():
+    midiout = rtmidi.MidiOut()
+    midiout.open_port(2) # FluidSynth virtual port (96411)
+    port_name = 'Synido TempoPAD Z-1'
+    with mido.open_input(port_name) as input_port:
+        print(f"Connected to MIDI input port: {port_name}")
+        while True:
+            await asyncio.sleep(0.01)
+            for msg in input_port.iter_pending():
+                print(f"Received MIDI message: {msg}")
+                midiout.send_message(msg.bytes())
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
