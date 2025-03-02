0f3ccb0c6163ac3ef761eb18b3a219d5b999bb73
Gene Boggs <gene@ology.net>
2025-03-01 22:09:20 -0800

    WIP

--- rtmidi-callback.pl
+++ rtmidi-callback.pl
@@ -342,7 +342,7 @@ sub walk_tone ($event) {
     return 0;
 }
 
-sub drum_parts ($note) {
+sub drum_part ($note) {
     # warn __PACKAGE__,' L',__LINE__,' ',,"N: $note\n";
     my $part;
     if ($note == 99) {
