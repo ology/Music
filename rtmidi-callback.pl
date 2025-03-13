267eb760d1e586a9805639095148a380e8371f37
Gene Boggs <gene@ology.net>
2025-03-13 13:57:17 -0700

    Move

--- rtmidi-callback.pl
+++ rtmidi-callback.pl
@@ -90,13 +90,13 @@ my $tka = Term::TermKey::Async->new(
         elsif ($pressed eq 't') { $rtfg->arp_type($arp_types->next); log_it(arp_type => $rtfg->arp_type) }
         elsif ($pressed eq 'm') { $rtfg->scale($scale_names->next); log_it(scale_name => $rtfg->scale) }
         elsif ($pressed eq 'u') { $rtfg->channel($channels->next); log_it(channel => $rtfg->channel) }
-        elsif ($pressed eq 'q') { $quantize = $quantize ? 0 : 1; log_it(quantize => $quantize) }
-        elsif ($pressed eq 'i') { $triplets = $triplets ? 0 : 1; log_it(triplets => $triplets) }
-        elsif ($pressed eq '-') { $direction = $direction ? 0 : 1; log_it(direction => $direction) }
         elsif ($pressed eq '!') { $rtfg->offset($rtfg->offset + ($direction ? 1 : -1)); log_it(offset => $rtfg->offset) }
         elsif ($pressed eq '@') { $rtfg->offset($rtfg->offset + ($direction ? 2 : -2)); log_it(offset => $rtfg->offset) }
         elsif ($pressed eq ')') { $rtfg->offset($rtfg->offset + ($direction ? 12 : -12)); log_it(offset => $rtfg->offset) }
         elsif ($pressed eq '(') { $rtfg->offset(0); log_it(offset => $rtfg->offset) }
+        elsif ($pressed eq 'q') { $quantize = $quantize ? 0 : 1; log_it(quantize => $quantize) }
+        elsif ($pressed eq 'i') { $triplets = $triplets ? 0 : 1; log_it(triplets => $triplets) }
+        elsif ($pressed eq '-') { $direction = $direction ? 0 : 1; log_it(direction => $direction) }
         elsif ($pressed eq ',') { $bpm += $direction ? 1  : -1;  log_it(bpm => $bpm) }
         elsif ($pressed eq '.') { $bpm += $direction ? 2  : -2;  log_it(bpm => $bpm) }
         elsif ($pressed eq '/') { $bpm += $direction ? 10 : -10; log_it(bpm => $bpm) }
