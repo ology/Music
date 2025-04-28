319ab01f730d8138ab2495f85f9561e1307aa022
Gene Boggs <gene@ology.net>
2025-04-28 10:42:28 -0700

    Initial

---
+++ rtmidi-vcv.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+use MIDI::RtController ();
+use MIDI::RtController::Filter::CC ();
+use Object::Destroyer ();
+
+my $input_name  = shift || 'keyboard'; # midi controller device
+my $output_name = shift || 'usb'; # midi output
+
+my $n = 5; # number of filters
+
+my @filters = get_filters(
+    port    => $input_name,
+    event   => [ ('control_change') x $n ],
+    trigger => [ (25) x $n ],
+    filters => [ ('scatter') x ($n - 1), 'flicker' ],
+    init_time => 1,
+    time_incr => 0.25,
+);
+
+# open the inputs
+my $controllers = MIDI::RtController::open_controllers([$input_name], $output_name, 1);
+
+MIDI::RtController::Filter::CC::add_filters(\@filters, $controllers);
+
+$controllers->{$input_name}->run; # and now trigger a MIDI message!
+
+# XXX maybe needed?
+END: {
+    for my $i (keys %$controllers) {
+        Object::Destroyer->new($controllers->{$i}, 'delete');
+    }
+}
+
+sub get_filters {
+    my (%args) = @_;
+    my @filters;
+    my $t = $args{init_time};
+    for my $i (0 .. $args{filters}->$#*) {
+        push @filters, {
+            control   => $i,
+            port      => $args{port},
+            event     => $args{event}->[$i],
+            trigger   => $args{trigger}->[$i],
+            type      => $args{filters}->[$i],
+            time_step => $t,
+        };
+        $t += $args{time_incr};
+    };
+    return @filters;
+}
