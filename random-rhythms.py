a0ce9b9fa988abb91e2c6aa0e51bcbede62d0311
Gene Boggs <gene@ology.net>
2025-09-03 16:34:44 -0700

    Cleanup

--- random-rhythms.py
+++ random-rhythms.py
@@ -2,23 +2,23 @@ from music21 import *
 import random
 from random_rhythms import Rhythm
 
-rr = Rhythm(
-    measure_size=4
+r = Rhythm(
+    measure_size=5,
+    groups={1/3: 3},
 )
-motif = rr.motif()
+motif = r.motif()
 print(motif)
 
-sc1 = scale.WholeToneScale('C4')
+sc = scale.WholeToneScale('C4')
 # print(sc1.pitches)
-sig = '5/4' # time signature
 
 s = stream.Stream()
-s.append(meter.TimeSignature(sig))
+s.append(meter.TimeSignature('5/4'))
 
-for dura in motif:
-    k = random.choice(sc1.pitches)
-    n = note.Note(k)
-    n.duration = duration.Duration(dura)
+for d in motif:
+    p = random.choice(sc.pitches)
+    n = note.Note(p)
+    n.duration = duration.Duration(d)
     # print(n.duration.type)
     s.append(n)
 
