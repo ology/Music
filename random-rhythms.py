0e187950ae0e10f197099447620c20c3e635f1f6
Gene Boggs <gene@ology.net>
2025-09-03 15:40:14 -0700

    Fix

--- random-rhythms.py
+++ random-rhythms.py
@@ -2,20 +2,9 @@ from music21 import *
 import random
 from random_rhythms import Rhythm
 
-# 128th to 128: .03125 .0625 .125 .25 .5 1 2 4 8 16 32 64 128
-# durations = [2**x for x in range(-5, 8)]
-# 16th to 16th: .25 .5 1 2 4 8 16
-# durations = [2**x for x in range(-2, 5)]
-# Triplets: .167 .333 .667 1.333 2.667
-# durations = [2**x/3 for x in range(-1, 4)]
-# Dotted: .375 .75 1.5 3 6
-# durations = [2**x+2**x/2 for x in range(-2, 3)]
-# Double dotted: .4375 .875 1.75 3.5 7
-# durations = [2**x+2**x/2+2**x/4 for x in range(-2, 3)]
-# 16th to whole: .25 .5 1 2 4
-# durations = [ 2**x for x in range(-2, 3) ]
-
-rr = Rhythm()
+rr = Rhythm(
+    measure_size=4
+)
 motif = rr.motif()
 print(motif)
 
