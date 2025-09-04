9c17f4e9a8982386f5d103cc0a3d4543deb4b6ad
Gene Boggs <gene@ology.net>
2025-09-03 22:40:09 -0700

    Improve

--- random-rhythms.py
+++ random-rhythms.py
@@ -1,12 +1,13 @@
 from music21 import *
+import random
 from random_rhythms import Rhythm
 
 r = Rhythm(
     measure_size=5,
     groups={1/3: 3},
 )
-motif = r.motif()
-print(motif)
+motifs = [ r.motif() for x in range(4) ]
+print(motifs)
 
 sc = scale.WholeToneScale('C4')
 # print(sc1.pitches)
@@ -14,11 +15,12 @@ sc = scale.WholeToneScale('C4')
 s = stream.Stream()
 s.append(meter.TimeSignature('5/4'))
 
-for d in motif:
-    p = random.choice(sc.pitches)
-    n = note.Note(p)
-    n.duration = duration.Duration(d)
-    # print(n.duration.type)
-    s.append(n)
+for m in motifs:
+    for d in m:
+        p = random.choice(sc.pitches)
+        n = note.Note(p)
+        n.duration = duration.Duration(d)
+        # print(n.duration.type)
+        s.append(n)
 
 s.show() # 'midi' opens Logic
