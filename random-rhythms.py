d1b1227f213229034f1346650cac3fcb8202dcbd
Gene Boggs <gene@ology.net>
2025-09-04 00:48:30 -0700

    VoicGen

--- random-rhythms.py
+++ random-rhythms.py
@@ -1,6 +1,20 @@
 from music21 import *
 import random
 from random_rhythms import Rhythm
+import subprocess
+
+def pitch_phrase(n, pitches, intervals=[-4,-3,-2,-1,1,2,3,4]):
+    try:
+        result = subprocess.run(['perl', 'voicegen.pl', str(n), str(pitches), str(intervals)], capture_output=True, text=True, check=True)
+        print(result.stdout)
+        if result.stderr:
+            print("Error output:")
+            print(result.stderr)
+        else:
+            return(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with error: {e}")
+        print(f"Stderr: {e.stderr}")
 
 r = Rhythm(
     measure_size=5,
@@ -10,15 +24,16 @@ motifs = [ r.motif() for x in range(4) ]
 # print(motifs)
 
 sc = scale.WholeToneScale('C4')
-# print(sc1.pitches)
+midinums = [ p.midi for p in sc.pitches ]
 
 s = stream.Stream()
 s.append(meter.TimeSignature('5/4'))
 
 for m in motifs:
-    for d in m:
-        p = random.choice(sc.pitches)
-        n = note.Note(p)
+    length=len(m)
+    p = pitch_phrase(length, midinums).split(',')
+    for i, d in enumerate(m):
+        n = note.Note(p[i])
         n.duration = duration.Duration(d)
         # print(n.duration.type)
         s.append(n)
