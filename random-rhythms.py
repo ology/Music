ae15c39d9a4d93cfa7b7ec765f4fce0ae055baab
Gene Boggs <gene@ology.net>
2025-09-05 08:13:27 -0700

    No

--- random-rhythms.py
+++ random-rhythms.py
@@ -1,4 +1,4 @@
-import random
+# import random
 from music21 import *
 from random_rhythms import Rhythm
 from music_voicegen import MusicVoiceGen
@@ -22,11 +22,11 @@ s.append(meter.TimeSignature('5/4'))
 
 for m in motifs:
     for i, d in enumerate(m):
-        chance = random.random()
-        if chance < 0.3:
-            n = note.Rest()
-        else:
-            n = note.Note(voice.rand())
+        # chance = random.random()
+        # if chance < 0.3:
+            # n = note.Rest()
+        # else:
+        n = note.Note(voice.rand())
         n.duration = duration.Duration(d)
         # print(n.duration.type)
         s.append(n)
