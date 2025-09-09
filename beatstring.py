653f1c196f739f43033d85edde058fbbff5c935d
Gene Boggs <gene@ology.net>
2025-09-09 00:40:52 -0700

    Initial

---
+++ beatstring.py
@@ -0,0 +1,30 @@
+from music_drummer import Drummer
+from music21 import instrument
+
+d = Drummer()
+
+d.set_instrument('kick', 'kick2')
+d.set_instrument('snare', 'snare2')
+d.set_instrument('crash', 'crash1')
+
+d.set_ts()
+d.set_bpm(60)
+
+d.count_in()
+d.rest('kick', duration=4)
+d.rest('snare', duration=4)
+d.rest('crash', duration=4)
+
+for _ in range(4):
+    d.pattern(
+        patterns={
+            'kick': '1000000010000000',
+            'snare': '0000100000001000',
+            'hihat': '1111111111111111',
+            'crash': '1000000000000000',
+        }
+    )
+
+d.sync_parts()
+
+d.score.show('midi')
