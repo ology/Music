dc48099a55eee3f7a47bf9937274284597ff8206
Gene Boggs <gene@ology.net>
2025-09-10 13:35:08 -0700

    Crashes

--- beatstring.py
+++ beatstring.py
@@ -17,15 +17,6 @@ d.note('pedal', duration=1/2)
 d.note('closed', duration=1/2)
 d.rest(['snare', 'kick'], duration=2)
 
-# d.note('crash1', duration=1/2)
-# d.note('crash2', duration=1/2)
-# d.note('china', duration=1/2)
-# d.note('splash', duration=1/2)
-# d.note('ride1', duration=1/2)
-# d.note('ride2', duration=1/2)
-# d.note('ridebell', duration=1/2)
-# d.rest(['kick', 'snare', 'hihat'], duration=3.5)
-
 for _ in range(4):
     d.pattern(
         patterns={
