319e3def805b0cc6443d9288819440f911160adc
Gene Boggs <gene@ology.net>
2025-09-14 20:54:02 -0700

    WIP

--- beatstring.py
+++ beatstring.py
@@ -3,9 +3,14 @@ from music_drummer import Drummer
 from find_primes import all_primes
 import random
 
-def pattern1(n=8):
+beats = 16
+
+def pattern1(rhythm, drummer, n=8):
+    kick  = ''.join([str(n) for n in rhythm.euclid(2, beats)])
+    snare = ''.join([str(n) for n in rhythm.rotate_n(4, rhythm.euclid(2, beats))])
+    hihat = ''.join([str(n) for n in rhythm.euclid(11, beats)])
     for _ in range(n):
-        d.pattern(
+        drummer.pattern(
             patterns={
                 'kick': kick,
                 'snare': snare,
@@ -13,40 +18,41 @@ def pattern1(n=8):
             }
         )
 
-def pattern2(n=4):
+def pattern2(rhythm, drummer, primes, n=4):
     for _ in range(n):
-        d.pattern(
+        drummer.pattern(
             patterns={
-                'kick':  ''.join([str(n) for n in r.euclid(random.choice(primes), beats)]),
-                'snare': ''.join([str(n) for n in r.euclid(random.choice(primes), beats)]),
-                'hihat': ''.join([str(n) for n in r.euclid(random.choice(primes), beats)]),
+                'kick':  ''.join([str(n) for n in rhythm.euclid(random.choice(primes), beats)]),
+                'snare': ''.join([str(n) for n in rhythm.euclid(random.choice(primes), beats)]),
+                'hihat': ''.join([str(n) for n in rhythm.euclid(random.choice(primes), beats)]),
             }
         )
 
-r = Rhythms()
-beats = 16
-kick  = ''.join([str(n) for n in r.euclid(2, beats)])
-snare = ''.join([str(n) for n in r.rotate_n(4, r.euclid(2, beats))])
-hihat = ''.join([str(n) for n in r.euclid(11, beats)])
+def main():
+    r = Rhythms()
 
-primes = all_primes(beats, 'list')
+    primes = all_primes(beats, 'list')
 
-d = Drummer()
+    d = Drummer()
 
-d.set_instrument('kick', 'kick2')
-d.set_instrument('snare', 'snare2')
+    d.set_instrument('kick', 'kick2')
+    d.set_instrument('snare', 'snare2')
 
-d.set_ts()
-d.set_bpm(100)
+    d.set_ts()
+    d.set_bpm(100)
 
-d.count_in()
-d.rest(['kick', 'snare'], duration=4)
+    d.count_in()
 
-pattern1()
-pattern2()
-pattern1()
+    d.rest(['kick', 'snare'], duration=4)
 
-d.sync_parts()
+    pattern1(r, d)
+    pattern2(r, d, primes)
+    pattern1(r, d)
 
-d.score.show('midi')
-# d.score.write(filename='drums.mid')
+    d.sync_parts()
+
+    d.score.show('midi')
+    # d.score.write(filename='drums.mid')
+
+if __name__ == "__main__":
+    main()
