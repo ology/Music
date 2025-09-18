706efe4cb90d6f007fb7f88b00459ec779418920
Gene Boggs <gene@ology.net>
2025-09-18 12:15:45 -0700

    WTF

--- tonnetz.py
+++ tonnetz.py
@@ -1,14 +1,20 @@
+from music21 import duration, chord, stream
 from music_tonnetztransform import Transform
-from music21 import chord, stream
+from random_rhythms import Rhythm
 
 s = stream.Stream()
 p = stream.Part()
 
-t = Transform()
-generated = t.generate()[0]
+r = Rhythm(durations=[1, 3/2, 2])
+motif = r.motif()
 
-for notes in generated:
-    c = chord.Chord(notes, type='whole')
+t = Transform(max=len(motif))
+generated = t.generate()[0]
+print(generated)
+
+for i,dura in enumerate(motif):
+    c = chord.Chord(generated[i])
+    c.duration = duration.Duration(dura)
     p.append(c)
 
 s.append(p)
