ec117371d378868808448d148d337419ac40cc04
Gene Boggs <gene@ology.net>
2025-09-18 13:52:20 -0700

    Try

--- tonnetz.py
+++ tonnetz.py
@@ -1,4 +1,5 @@
 from music21 import duration, chord, stream
+from chord_progression_network import Generator
 from music_tonnetztransform import Transform
 from random_rhythms import Rhythm
 
@@ -6,13 +7,22 @@ s = stream.Stream()
 p = stream.Part()
 
 r = Rhythm(durations=[1, 3/2, 2])
-motif = r.motif()
+motifs = [ r.motif() for _ in range(3) ]
 
-t = Transform(max=len(motif))
-generated = t.generate()[0]
-print(generated)
+t = Transform(max=len(motifs[0]))
+generated = t.circular()[0]
 
-for i,dura in enumerate(motif):
+g = Generator()
+
+for motif in motifs:
+    g.max = len(motif)
+    phrase = g.generate()
+    for i,dura in enumerate(motif):
+        c = chord.Chord(phrase[i])
+        c.duration = duration.Duration(dura)
+        p.append(c)
+
+for i,dura in enumerate(motifs[0]):
     c = chord.Chord(generated[i])
     c.duration = duration.Duration(dura)
     p.append(c)
