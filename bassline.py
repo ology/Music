c9d8e0a6540b7623c7a16f384a822d8745d73c7d
Gene Boggs <gene@ology.net>
2025-09-19 22:37:56 -0700

    Initial

---
+++ bassline.py
@@ -0,0 +1,64 @@
+from music21 import note, stream
+from music_bassline_generator import Bassline
+
+def add_notes(s, notes):
+    for n in notes:
+        n = note.Note(n, type='quarter')
+    s.append(n)
+
+s = stream.Stream()
+bass_part = stream.Part()
+
+bass = Bassline()
+
+num = 4
+
+# Autumn Leaves verse
+for chord in ['Am7','D7','GM7','CM7','F#m7b5','B7','Em','Em']:
+    notes = bass.generate(chord, num)
+    add_notes(bass_part, notes)
+
+s.insert(0, bass_part)
+
+s.show()
+
+"""
+bass = Bassline(
+    keycenter='C', # tonic for modal accompaniment
+    modal=False, # only choose notes within the mode
+    chord_notes=True, # use chord notes outside the scale
+    intervals=[-3, -2, -1, 1, 2, 3], # allowed voicegen intervals
+    octave=1, # lowest MIDI octave
+    tonic=False, # play the first scale note to start the generated phrase
+    positions=None, # allowed notes for major and minor scales
+    guitar=False, # transpose notes below E1 (midi #28) up an octave
+    wrap=None, # transpose notes above this ISO named note, down an octave
+    format='ISO',
+    verbose=True, # show progress
+)
+
+notes = bass.generate('C7b5', 4)
+print(notes)
+
+notes = bass.generate('D/A', 4)
+notes = bass.generate('D', 4, 'C/G')
+notes = bass.generate('D', 1)
+
+bass = Bassline(modal=True)
+notes = bass.generate('Dm7')
+notes = bass.generate('Dm7b5')
+
+bass = Bassline(
+    octave=3,
+    wrap='C3',
+    modal=True,
+)
+notes = bass.generate('C', 4)
+
+bass = Bassline(
+    # chord_notes=False,
+    positions={'major': [x for x in range(6)], 'minor': [x for x in range(6)]} # no 7ths!
+)
+notes = bass.generate('C', 4)
+print(notes)
+"""
