12a0647b703c1cdc85242210f223ae080d1f4420
Gene Boggs <gene@ology.net>
2025-09-22 22:45:57 -0700

    Initial

---
+++ arpeggiate.py
@@ -0,0 +1,46 @@
+from chord_progression_network import Generator
+from music_melodicdevice import Device
+from music21 import duration, note, stream
+from pychord import Chord
+import re
+
+s = stream.Score()
+p = stream.Part()
+
+g = Generator(
+    scale_name='whole-tone scale',
+    net={
+        1: [2,3,4,5,6],
+        2: [1,3,4,5,6],
+        3: [1,2,4,5,6],
+        4: [1,2,3,5,6],
+        5: [1,2,3,4,6],
+        6: [1,2,3,4,5],
+    },
+    chord_map=['m'] * 6,
+    substitute=True,
+    # verbose=True,
+)
+
+phrase = g.generate()
+
+device = Device(verbose=True)
+
+for ph in phrase:
+    for c in ph:
+        m = re.search(r'^[A-G][#b]?(\d)$', c)
+        if m:
+            octave = int(m.group(1))
+        else:
+            octave = 0
+        ch = Chord(c)
+        components = ch.components_with_pitch(octave)
+        arped = device.arp(components, duration=1, arp_type='up')
+        for a in arped:
+            n = note.Note(a[1])
+            n.duration = duration.Duration(a[0])
+            p.append(n)
+
+s.append(p)
+
+s.show()
