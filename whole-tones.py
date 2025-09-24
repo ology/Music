ffe77a15d3c00ea6f17924a33de6f8ce633b85a2
Gene Boggs <gene@ology.net>
2025-09-23 20:52:17 -0700

    WIP

--- whole-tones.py
+++ whole-tones.py
@@ -1,5 +1,5 @@
 from music21 import duration, chord, note, scale, stream
-from music_bassline_generator import Bassline
+# from music_bassline_generator import Bassline
 from chord_progression_network import Generator
 from music_tonnetztransform import Transform
 from music_voicegen import MusicVoiceGen
@@ -9,7 +9,9 @@ import random
 s = stream.Stream()
 chord_part = stream.Part()
 melody_part = stream.Part()
-# bass_part = stream.Part()
+bass_part = stream.Part()
+
+bass_notes = [] # accumulated by the note of a chord progression measure
 
 r = Rhythm(durations=[1, 2, 3])
 chord_motifs = [ r.motif() for _ in range(3) ]
@@ -42,6 +44,7 @@ for _ in range(2):
         g.tonic = i == 0
         g.resolve = i == len(motif) - 1
         phrase = g.generate()
+        bass_notes.append(phrase[0][0])
         for j,dura in enumerate(motif):
             c = chord.Chord(phrase[j])
             c.duration = duration.Duration(dura)
@@ -53,6 +56,7 @@ for _ in range(2):
         verbose=False,
     )
     generated = t.circular()[0]
+    bass_notes.append('rest')
     for i,dura in enumerate(chord_motifs[0]):
         c = chord.Chord(generated[i])
         c.duration = duration.Duration(dura)
@@ -60,6 +64,7 @@ for _ in range(2):
     for motif in chord_motifs + [chord_motifs[0]]:
         g.max = len(motif)
         phrase = g.generate()
+        bass_notes.append(phrase[0][0])
         for j,dura in enumerate(motif):
             c = chord.Chord(phrase[j])
             c.duration = duration.Duration(dura)
@@ -83,7 +88,7 @@ for _ in range(2):
     melody_part.append(n)
     for motif in melody_motifs + [melody_motifs[0]]:
         for dura in motif:
-            if random.random() < 0.2:
+            if random.random() < 0.1:
                 n = note.Rest()
             else:
                 n = note.Note(v.rand())
@@ -96,13 +101,16 @@ for _ in range(2):
 # )
 # notes = bass.generate('C', 14)
 
-# for _ in range(2):
-#     for n in notes:
-#         n = note.Note(n, type='half')
-#         bass_part.append(n)
+
+for n in bass_notes:
+    if n == 'rest':
+        n = note.Rest(type='whole')
+    else:
+        n = note.Note(n, type='whole')
+    bass_part.append(n)
 
 s.insert(0, chord_part)
 s.insert(0, melody_part)
-# s.insert(0, bass_part)
+s.insert(0, bass_part.transpose(-(12 * 2)))
 
 s.show()
