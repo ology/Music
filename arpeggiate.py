08678f66dc84924aabf2de0de7a3e177db00184a
Gene Boggs <gene@ology.net>
2025-09-23 22:43:11 -0700

    WIP

--- arpeggiate.py
+++ arpeggiate.py
@@ -25,8 +25,7 @@ phrase = g.generate()
 device = Device(verbose=False)
 
 for i, ph in enumerate(phrase):
-    arp_type = 'up' if i % 2 == 0 else 'down'
-    arped = device.arp(ph, duration=1, arp_type=arp_type, repeats=1)
+    arped = device.arp(ph, duration=1, arp_type='updown', repeats=1)
     for a in arped:
         n = note.Note(a[1])
         n.duration = duration.Duration(a[0])
