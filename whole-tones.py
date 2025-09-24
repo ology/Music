133116b01a2f4b156a6a9ea0df6d3de590f56ab7
Gene Boggs <gene@ology.net>
2025-09-23 20:59:07 -0700

    WIP

--- whole-tones.py
+++ whole-tones.py
@@ -26,11 +26,11 @@ melody_motifs = [ r.motif() for _ in range(3) ]
 g = Generator(
     scale_name='whole-tone scale',
     net={
-        1: [2,3,4,5,6],
-        2: [1,3,4,5,6],
+        1: [2],
+        2: [5],
         3: [1,2,4,5,6],
         4: [1,2,3,5,6],
-        5: [1,2,3,4,6],
+        5: [1],
         6: [1,2,3,4,5],
     },
     chord_map=['m'] * 6, # every chord is the same flavor
