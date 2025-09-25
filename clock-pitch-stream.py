943e888a2026b5642b810a7a321550e6e368feb7
Gene Boggs <gene@ology.net>
2025-09-25 14:36:35 -0700

    WIP

--- clock-pitch-stream.py
+++ clock-pitch-stream.py
@@ -1,6 +1,9 @@
 import mido
 import time
 import threading
+from music21 import pitch
+from chord_progression_network import Generator
+from music_melodicdevice import Device
 
 MIDI_PORT_NAME = 'USB MIDI Interface'
 BPM = 100
@@ -8,9 +11,29 @@ NOTE_DURATION_SECONDS = 0.5
 NOTE_VELOCITY = 100
 CLOCK_DELAY = (60 / BPM) / 24 # 24 clock messages per quarter note
 
+weights = [ 1 for _ in range(1,6) ] # equal probability
+g = Generator(
+    max=4 * 6, # beats x measures
+    scale_name='whole-tone scale',
+    net={
+        1: [2,3,4,5,6],
+        2: [1,3,4,5,6],
+        3: [1,2,4,5,6],
+        4: [1,2,3,5,6],
+        5: [1,2,3,4,6],
+        6: [1,2,3,4,5],
+    },
+    weights={ i: weights for i in range(1,7) },
+    chord_map=['7'] * 6, # set every chord to the same flavor (like '', 'm', '7')
+    resolve=False,
+    substitute=True,
+    verbose=False,
+)
+phrase = g.generate()
+device = Device(verbose=False)
+
 try:
     outport = mido.open_output(MIDI_PORT_NAME)
-    # print(f"Opened MIDI port: {MIDI_PORT_NAME}")
 except Exception as e:
     print(f"Error opening MIDI port: {e}")
     exit()
@@ -21,14 +44,16 @@ def midi_clock_thread():
         time.sleep(CLOCK_DELAY)
 
 def note_stream_thread():
-    notes_to_play = [60, 62, 64, 65, 67, 69, 71, 72] # C major scale    
-    for note in notes_to_play:
-        msg_on = mido.Message('note_on', note=note, velocity=NOTE_VELOCITY)
-        outport.send(msg_on)
-        time.sleep(NOTE_DURATION_SECONDS) # Hold note for a duration
-        msg_off = mido.Message('note_off', note=note, velocity=NOTE_VELOCITY)
-        outport.send(msg_off)
-        time.sleep(0.1) # Small delay between notes
+    for ph in phrase:
+        arped = device.arp(ph, duration=1, arp_type='updown', repeats=1)
+        for a in arped:
+            p = pitch.Pitch(a[1]).midi
+            msg_on = mido.Message('note_on', note=p, velocity=NOTE_VELOCITY)
+            outport.send(msg_on)
+            time.sleep(a[0])
+            msg_off = mido.Message('note_off', note=p, velocity=NOTE_VELOCITY)
+            outport.send(msg_off)
+            # time.sleep(0.1) # delay between notes
 
 if __name__ == "__main__":
     clock_thread = threading.Thread(target=midi_clock_thread, daemon=True) # Daemon so it stops when main thread exits
