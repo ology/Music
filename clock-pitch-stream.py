dac82d39bfb3275b7591dbd10da23fdb80a6e114
Gene Boggs <gene@ology.net>
2025-09-25 14:39:42 -0700

    WIP

--- clock-pitch-stream.py
+++ clock-pitch-stream.py
@@ -5,11 +5,10 @@ from music21 import pitch
 from chord_progression_network import Generator
 from music_melodicdevice import Device
 
-MIDI_PORT_NAME = 'USB MIDI Interface'
-BPM = 100
-NOTE_DURATION_SECONDS = 0.5
-NOTE_VELOCITY = 100
-CLOCK_DELAY = (60 / BPM) / 24 # 24 clock messages per quarter note
+midi_port = 'USB MIDI Interface'
+bpm = 100
+velocity = 100
+interval = (60 / bpm) / 24 # 24 clock messages per quarter note
 
 weights = [ 1 for _ in range(1,6) ] # equal probability
 g = Generator(
@@ -33,7 +32,7 @@ phrase = g.generate()
 device = Device(verbose=False)
 
 try:
-    outport = mido.open_output(MIDI_PORT_NAME)
+    outport = mido.open_output(midi_port)
 except Exception as e:
     print(f"Error opening MIDI port: {e}")
     exit()
@@ -41,27 +40,24 @@ except Exception as e:
 def midi_clock_thread():
     while True:
         outport.send(mido.Message('clock'))
-        time.sleep(CLOCK_DELAY)
+        time.sleep(interval)
 
 def note_stream_thread():
     for ph in phrase:
         arped = device.arp(ph, duration=1, arp_type='updown', repeats=1)
         for a in arped:
             p = pitch.Pitch(a[1]).midi
-            msg_on = mido.Message('note_on', note=p, velocity=NOTE_VELOCITY)
+            msg_on = mido.Message('note_on', note=p, velocity=velocity)
             outport.send(msg_on)
             time.sleep(a[0])
-            msg_off = mido.Message('note_off', note=p, velocity=NOTE_VELOCITY)
+            msg_off = mido.Message('note_off', note=p, velocity=velocity)
             outport.send(msg_off)
             # time.sleep(0.1) # delay between notes
 
 if __name__ == "__main__":
     clock_thread = threading.Thread(target=midi_clock_thread, daemon=True) # daemon = stops when main thread exits
     note_thread = threading.Thread(target=note_stream_thread)
-
     clock_thread.start()
     note_thread.start()
-
     note_thread.join() # wait for the note stream to finish
-
     outport.close()
