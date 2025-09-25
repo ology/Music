6706f91b180fa73ece9a89eecdd208b46c10afa8
Gene Boggs <gene@ology.net>
2025-09-25 14:25:19 -0700

    WIP

--- clock-pitch-stream.py
+++ clock-pitch-stream.py
@@ -5,7 +5,7 @@ import threading
 MIDI_PORT_NAME = 'USB MIDI Interface'
 BPM = 100
 NOTE_DURATION_SECONDS = 0.5
-NOTE_VELOCITY = 64
+NOTE_VELOCITY = 100
 
 # Calculate delay for MIDI clock (24 clock messages per quarter note)
 CLOCK_DELAY = (60 / BPM) / 24
