0e0208c71c4c720458c7bad964f64e674110419a
Gene Boggs <gene@ology.net>
2025-10-07 23:16:15 -0700

    Fresh

---
+++ arpeggiate_nicely.py
@@ -0,0 +1,40 @@
+from chord_progression_network import Generator
+from music_melodicdevice import Device
+from music21 import duration, note, stream
+
+s = stream.Score()
+p = stream.Part()
+
+weights = [ 1 for _ in range(1,5) ] # equal probability
+
+g = Generator(
+    max=4 * 4, # beats x measures
+    # scale_name='whole-tone scale',
+    net={
+        1: [2,3,5,6],
+        2: [1,3,5,6],
+        3: [1,2,5,6],
+        4: [1,1,1,1],
+        5: [1,2,3,6],
+        6: [1,2,3,5],
+        7: [1,1,1,1],
+    },
+    weights={ i: weights for i in range(1,8) },
+    chord_map=['m','','m','','m','','m'],
+    resolve=False,
+    substitute=False,
+    verbose=False,
+)
+phrase = g.generate()
+
+device = Device(verbose=False)
+
+for i, ph in enumerate(phrase):
+    arped = device.arp(ph, duration=1, arp_type='updown', repeats=1)
+    for a in arped:
+        n = note.Note(a[1])
+        n.duration = duration.Duration(a[0])
+        p.append(n)
+
+s.append(p)
+s.show('midi')
