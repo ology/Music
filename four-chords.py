f2022e9d2774d19aa130ec4b6188ad255ba98b13
Gene Boggs <gene@ology.net>
2025-10-16 00:22:01 -0700

    WIP

--- four-chords.py
+++ four-chords.py
@@ -1,11 +1,6 @@
 from music21 import chord, duration, stream
 from random_rhythms import Rhythm
-try:
-    import sys
-    sys.path.append('./src')
-    from chord_progression_network.chord_progression_network import Generator
-except ImportError:
-    from chord_progression_network import Generator
+from chord_progression_network import Generator
 
 r = Rhythm(durations=[1, 3/2, 2])
 motifs = [ r.motif() for _ in range(4) ]
@@ -13,15 +8,29 @@ motifs = [ r.motif() for _ in range(4) ]
 s = stream.Score()
 p = stream.Part()
 
-g = Generator()
+g1 = Generator(
+    net={
+        1: [5],
+        2: [],
+        3: [],
+        4: [],
+        5: [6],
+        6: [4],
+        7: [],
+    },
+    weights={ i: [1] for i in range(1, 8) },
+    tonic=False,
+    resolve=False,
+)
 
-for m in motifs:
-    g.max = len(m)
-    phrase = g.generate()
-    for i, d in enumerate(m):
-        c = chord.Chord(phrase[i])
-        c.duration = duration.Duration(d)
-        p.append(c)
+for _ in range(4):
+    for m in motifs:
+        g1.max = len(m)
+        phrase = g1.generate()
+        for i, d in enumerate(m):
+            c = chord.Chord(phrase[i])
+            c.duration = duration.Duration(d)
+            p.append(c)
 
 s.append(p)
 
