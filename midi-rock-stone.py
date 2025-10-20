d3d546f69e17024b5691cd3fe883ce07203e121b
Gene Boggs <gene@ology.net>
2025-10-20 01:04:11 -0700

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -2,6 +2,7 @@ import mido # also install python-rtmidi
 import os
 import re
 import sys
+import time
 import yaml
 
 out_port_name = sys.argv[3] if len(sys.argv) > 3 else 'SE-02'
@@ -19,6 +20,13 @@ with open(device_file, 'r') as f:
     data = yaml.safe_load(f)
     print(data)
 
+def midi_message(outport, channel, note, sleep):
+    msg = mido.Message('note_on', note=note, velocity=100, channel=channel)
+    outport.send(msg)
+    time.sleep(sleep)
+    msg = mido.Message('note_off', note=note, velocity=100, channel=channel)
+    outport.send(msg)
+
 try:
     with mido.open_input(in_port_name) as inport:
         print(f"Listening to: {inport.name}")
@@ -26,9 +34,11 @@ try:
             for msg in inport:
                 if msg.type != 'clock':
                     if msg.type == 'control_change' and msg.control == 26 and msg.value == 0:
-                        print(f"Received: {msg}")
+                        # print(f"Received: {msg}")
+                        midi_message(outport, 0, 60, 1)
                     elif msg.type == 'control_change' and msg.control == 26 and msg.value == 127:
-                        print(f"Received: {msg}")
+                        # print(f"Received: {msg}")
+                        midi_message(outport, 0, 67, 1)
 except KeyboardInterrupt:
     print("Stopping MIDI input.")
 except Exception as e:
