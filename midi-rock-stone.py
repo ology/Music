f2b5ed9d39e8e8ddddd3ebb4adfd8e7d3a36d988
Gene Boggs <gene@ology.net>
2025-10-20 00:35:39 -0700

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -4,6 +4,7 @@ import re
 import sys
 import yaml
 
+out_port_name = sys.argv[3] if len(sys.argv) > 3 else 'SE-02'
 in_port_name = sys.argv[2] if len(sys.argv) > 2 else 'MIDI ROCK Joystick'
 device_file = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0]
 
