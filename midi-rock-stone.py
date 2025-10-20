b7af7db747e28fb7ef3b2b8ec6a504d50eb0c8cb
Gene Boggs <gene@ology.net>
2025-10-19 23:20:17 -0700

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -8,9 +8,9 @@ match = re.search(r'^(.+?)\.py$', device_file)
 if match:
     device_file = match.group(1)
 device_file = device_file + '.yaml'
-if os.path.exists(device_file):
-    with open(device_file, 'r') as f:
-        data = yaml.safe_load(f)
-        print(data)
-else:
+if not os.path.exists(device_file):
     print(device_file, 'does not exist')
+
+with open(device_file, 'r') as f:
+    data = yaml.safe_load(f)
+    print(data)
