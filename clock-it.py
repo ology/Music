e2854dbcf55d666b5397497fd61ed87feb5d63c8
Gene Boggs <gene@ology.net>
2025-10-22 09:31:37 -0700

    WIP

--- clock-it.py
+++ clock-it.py
@@ -2,9 +2,8 @@ import mido
 import sys
 import time
 
-def send_clock(outport, bpm=100):
-    # Calculate the time between clock messages (24 PPQN per beat)
-    interval = 60 / (bpm * 24)
+def send_clock(outport, bpm):
+    interval = 60 / (int(bpm) * 24) # time between clock messages (24 PPQN per beat)
     try:
         while True:
             outport.send(mido.Message('clock'))
@@ -18,5 +17,6 @@ if __name__ == "__main__":
     bpm = sys.argv[2] if len(sys.argv) > 2 else 120
     port = sys.argv[1] if len(sys.argv) > 1 else 'USB MIDI Interface'
     with mido.open_output(port) as outport:
+        print(outport, f"at {bpm} BPM")
         outport.send(mido.Message('start'))
         send_clock(outport, bpm)
