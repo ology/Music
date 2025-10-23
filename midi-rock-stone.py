600e7936f3affc32123438735b65f480f73f2959
Gene Boggs <gene@ology.net>
2025-10-22 17:36:18 -0700

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -5,8 +5,6 @@ import sys
 import time
 import yaml
 
-out_port_name = sys.argv[3] if len(sys.argv) > 3 else 'SE-02'
-in_port_name = sys.argv[2] if len(sys.argv) > 2 else 'MIDI ROCK Joystick'
 device_file = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0]
 
 match = re.search(r'^(.+?)\.py$', device_file)
@@ -37,21 +35,10 @@ def send_to(outport, mtype, patch=0, data=0, channel=0, velocity=100):
         msg = mido.Message('note_off', note=patch, velocity=velocity, channel=channel)
         outport.send(msg)
 
-def get_by_value(dictionary, target_value):
-    found = False
-    for item in dictionary['messages']:
-        for value in item.values():
-            if value == target_value:
-                found = True
-                break
-        if found:
-            break
-    return item
-
 with open(device_file, 'r') as f:
     data = yaml.safe_load(f)
-    start = get_by_value(data, 'green button')['patch']
-    stop = get_by_value(data, 'red button')['patch']
+    in_port_name = data['controller']
+    out_port_name = data['device']
 
 try:
     with mido.open_input(in_port_name) as inport:
@@ -62,16 +49,16 @@ try:
                 if msg.type == 'clock':
                     continue
                 print(f"Received: {msg}")
-                if msg.type == 'note_on' and msg.note == start:
-                    send_to(outport, 'start')
-                elif msg.type == 'note_on' and msg.note == stop:
-                    send_to(outport, 'stop')
-                elif msg.type == 'control_change' and msg.control == 1:
-                    send_to(outport, 'control_change', msg.control, msg.value)
-                elif msg.type == 'pitchwheel':
-                    send_to(outport, 'pitchwheel', 0, msg.pitch)
-                elif msg.type == 'control_change' and msg.control == 9:
-                    send_to(outport, 'program_change', msg.value)
+                # if msg.type == 'note_on' and msg.note == start:
+                #     send_to(outport, 'start')
+                # elif msg.type == 'note_on' and msg.note == stop:
+                #     send_to(outport, 'stop')
+                # elif msg.type == 'control_change' and msg.control == 1:
+                #     send_to(outport, 'control_change', msg.control, msg.value)
+                # elif msg.type == 'pitchwheel':
+                #     send_to(outport, 'pitchwheel', 0, msg.pitch)
+                # elif msg.type == 'control_change' and msg.control == 9:
+                #     send_to(outport, 'program_change', msg.value)
 except KeyboardInterrupt:
     print('Stopping MIDI I/O.')
 except Exception as e:
