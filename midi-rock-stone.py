4fa3b003c6df568118e1b6e0c29731742c4c07a0
Gene Boggs <gene@ology.net>
2025-10-23 14:14:50 -0700

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -5,16 +5,6 @@ import sys
 import time
 import yaml
 
-device_file = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0]
-
-match = re.search(r'^(.+?)\.py$', device_file)
-if match:
-    device_file = match.group(1)
-    device_file = device_file + '.yaml'
-if not os.path.exists(device_file):
-    print(device_file, 'does not exist')
-    sys.exit()
-
 def send_to(outport, mtype, patch=0, data=0, channel=0, velocity=100):
     if mtype == 'start' or mtype == 'stop':
         msg = mido.Message(mtype)
@@ -51,22 +41,33 @@ def dispatch(port, msg, data):
             elif m['type'] == 'pitchwheel':
                 send_to(port, 'pitchwheel', data=msg.pitch)
 
-with open(device_file, 'r') as f:
-    data = yaml.safe_load(f)
-    in_port_name = data['controller']
-    out_port_name = data['device']
+if __name__ == "__main__":
+    device_file = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0]
 
-try:
-    with mido.open_input(in_port_name) as inport:
-        print('Listening to:', inport.name)
-        with mido.open_output(out_port_name) as outport:
-            print('Sending to:', outport.name)
-            for msg in inport:
-                if msg.type == 'clock':
-                    continue
-                print(f"Received: {msg}")
-                dispatch(outport, msg, data)
-except KeyboardInterrupt:
-    print('Stopping MIDI I/O.')
-except Exception as e:
-    print(f"ERROR: {e}")
+    match = re.search(r'^(.+?)\.py$', device_file)
+    if match:
+        device_file = match.group(1)
+        device_file = device_file + '.yaml'
+    if not os.path.exists(device_file):
+        print(device_file, 'does not exist')
+        sys.exit()
+
+    with open(device_file, 'r') as f:
+        data = yaml.safe_load(f)
+        in_port_name = data['controller']
+        out_port_name = data['device']
+
+    try:
+        with mido.open_input(in_port_name) as inport:
+            print('Listening to:', inport.name)
+            with mido.open_output(out_port_name) as outport:
+                print('Sending to:', outport.name)
+                for msg in inport:
+                    if msg.type == 'clock':
+                        continue
+                    print(f"Received: {msg}")
+                    dispatch(outport, msg, data)
+    except KeyboardInterrupt:
+        print('Stopping MIDI I/O.')
+    except Exception as e:
+        print(f"ERROR: {e}")
