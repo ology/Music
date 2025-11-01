961be0db6f99da3fe6f55e2d070c270a45ced576
Gene Boggs <gene@ology.net>
2025-11-01 13:52:45 -0700

    WIP

--- pump-notes.py
+++ pump-notes.py
@@ -1,10 +1,9 @@
 import mido
 import random
 import time
-import threading
 from music21 import pitch
 
-def note_stream_thread():
+def note_stream():
     notes = [ i for i in range(60, 73)]
     phrase = [ random.choice(notes) for _ in range(16) ]
     while True:
@@ -18,19 +17,12 @@ def note_stream_thread():
 
 if __name__ == "__main__":
     with mido.open_output('USB MIDI Interface') as outport:
-        print(outport)
-        note_thread = threading.Thread(target=note_stream_thread, daemon=True)
-        note_thread.start()
         outport.send(mido.Message('start'))
         try:
-            while True:
-                time.sleep(0.5) # keep main thread alive and respond to interrupts
+            note_stream()
         except KeyboardInterrupt:
             outport.send(mido.Message('stop'))
-            print("\nSignaling threads to stop...")
-            stop_threads = True
-            note_thread.join()
-            print("All threads stopped.")
+            print("\nStop!")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
         finally:
