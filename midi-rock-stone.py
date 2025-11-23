86501cf9162f835f962e793c8f9a483216d5237a
Gene Boggs <gene@ology.net>
2025-11-23 09:41:20 -0800

    WIP

--- midi-rock-stone.py
+++ midi-rock-stone.py
@@ -46,8 +46,29 @@ def dispatch(port, msg, data):
                 send_to(port, 'control_change', patch=m['target'], data=m['data'])
             elif m['type'] == 'control_change' and msg.control == m['control']:
                 send_to(port, 'control_change', patch=m['target'], data=msg.value)
+            elif m['type'] == 'pitchwheel' and m['cmd'] == 'control_change':
+                scaled_result = scale_number(msg.pitch, -8192, 8192, 0, 127)
+                send_to(port, 'control_change', patch=m['target'], data=scaled_result)
             elif m['type'] == 'pitchwheel':
                 send_to(port, 'pitchwheel', data=msg.pitch)
+
+def scale_number(value, original_min, original_max, target_min, target_max):
+    """
+    Scales a number from one range to another as an integer.
+    Args:
+        value: The number to be scaled.
+        original_min: minimum value of the original range
+        original_max: maximum value of the original range
+        target_min: minimum value of the target range
+        target_max: maximum value of the target range
+    Returns:
+        float: The integer scaled number in the target range
+    """
+    if original_max == original_min:
+        # Handle the case where the original range is a single point
+        return target_min  # Or raise an error, depending on desired behavior
+    scaled_value = ((value - original_min) * (target_max - target_min)) / (original_max - original_min) + target_min
+    return round(scaled_value)
 
 if __name__ == "__main__":
     device_file = sys.argv[1] if len(sys.argv) > 1 else sys.argv[0]
