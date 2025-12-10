e7b32d0e94bf083eeb3aa014745f40064e4344de
Gene Boggs <gene@ology.net>
2025-12-10 14:04:43 -0800

    Fresh

---
+++ midi-Chord2-control-default.py
@@ -0,0 +1,84 @@
+"""
+This program controls the QU-Bit Chord module through a multi-timbral MIDI-CV interface.
+
+MIDI channel 0 = chord quality cv input
+MIDI channel 1 = v/oct root note 
+
+harm: blue = default qualities
+0-5: maj, 6-13: min, 14-21: dom, 22-28: dim*, 29-36: dim,
+37-43: sus*, 44-51: sus, 52+: aug
+
+"""
+
+import sys
+import mido
+import random
+import time
+import threading
+from music_voicegen import MusicVoiceGen
+
+def play_chord(quality, note, velocity=127, duration=1):
+    msg = mido.Message('note_on', note=quality, channel=0, velocity=velocity)
+    outport.send(msg)
+    msg = mido.Message('note_on', note=quality, channel=1, velocity=velocity)
+    outport.send(msg)
+    time.sleep(duration * factor)
+    msg = mido.Message('note_off', note=note, channel=0, velocity=velocity)
+    outport.send(msg)
+    msg = mido.Message('note_off', note=note, channel=1, velocity=velocity)
+    outport.send(msg)
+
+def note_stream_thread():
+    global voice, pitch_qualites, quality_volts, stop_threads
+    while not stop_threads:
+        pitch = voice.rand()
+        quality = quality_volts[pitch_qualites[pitch]]
+        play_chord(quality, pitch, duration=4)
+
+if __name__ == "__main__":
+    pitch_qualites = {
+        60: 'maj',
+        62: 'min',
+        64: 'min',
+        65: 'maj',
+        67: 'maj',
+        69: 'min',
+        71: 'dim',
+    }
+    quality_volts = {
+        'maj': 0,
+        'min': 6,
+        'dom': 14,
+        'dim*': 22,
+        'dim': 29,
+        # 'sus*': 37,
+        # 'sus': 44,
+        # 'aug': 52,
+    }
+    voice = MusicVoiceGen(pitches=list(pitch_qualites.keys()))
+
+    factor = 1/2 # duration multiplier
+    # time between clocks at 24 PPQN per beat and 100 BPM
+    interval = 60 / (100 * 24)
+    stop_threads = False # should I stay or should I go?
+
+    port_name = sys.argv[1] if len(sys.argv) > 1 else 'MIDIThing2'
+
+    with mido.open_output(port_name) as outport:
+        print(outport)
+        note_thread = threading.Thread(target=note_stream_thread, daemon=True)
+        note_thread.start()
+        outport.send(mido.Message('start'))
+        try:
+            while True:
+                time.sleep(interval) # keep main thread alive and respond to interrupts
+        except KeyboardInterrupt:
+            outport.send(mido.Message('stop'))
+            print("\nSignaling threads to stop...")
+            stop_threads = True
+            note_thread.join()
+            print("All threads stopped.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        finally:
+            outport.close()
