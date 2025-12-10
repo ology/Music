732670fc6e8c6eeeebc6a66c2a03a1978e6dfd74
Gene Boggs <gene@ology.net>
2025-12-09 17:15:23 -0800

    WIP

--- midi-Chord2-control.py
+++ midi-Chord2-control.py
@@ -1,17 +1,34 @@
-# This program controls the QU-Bit Chord module through a multi-timbral MIDI-CV interface.
-#
-# MIDI channel 0 = chord quality cv input
-# MIDI channel 1 = v/oct root note 
-#
-# Default quality:
-# harm: blue = default qualities
-# 0-5: maj, 6-13: min, 14-21: dom, 22-28: dim*, 29-36: dim,
-# 37-43: sus*, 44-51: sus, 52+: aug
-#
-# User chord type:
-# harm: white = 12cuser types
-# 0-3: 1, 4-9: 2, 10-14: 3, 15-19: 4, 20-24: 5, 25-29: 6,
-# 30-34: 7, 35-39: 8, 40-44: 9, 45-49: 10, 50-54: 11, 55+: 12
+"""
+This program controls the QU-Bit Chord module through a multi-timbral MIDI-CV interface.
+
+MIDI channel 0 = chord quality cv input
+MIDI channel 1 = v/oct root note 
+
+Default quality:
+  harm: blue = default qualities
+  0-5: maj, 6-13: min, 14-21: dom, 22-28: dim*, 29-36: dim,
+  37-43: sus*, 44-51: sus, 52+: aug
+
+User chord type:
+  harm: white = 12 user defined types
+  0-3: 1, 4-9: 2, 10-14: 3, 15-19: 4, 20-24: 5, 25-29: 6,
+  30-34: 7, 35-39: 8, 40-44: 9, 45-49: 10, 50-54: 11, 55+: 12
+
+Here is a chord_config.txt example, where 5 of 12 user chord types are defined.
+The chords can each have up to 4 pitches. (And the root 0 is assumed to be present).
+The "#" comments below are for illustration only:
+
+QUANTIZE_AUTOHARM=1  # RTFM
+LONGFORM_USERBANK=0  # single-file waveforms or 8 x 8 banks
+LFO_MODE=0           # 6 octaves below audible
+LEAD_OFFSET=-1       # 0: default, 1: octave above, -1: octave below
+CHORD_1=4,7,11       # maj7
+CHORD_2=3,7,10       # min7
+CHORD_3=7            # 5 no 3
+CHORD_4=4,7,9        # 6th
+CHORD_5=5,7          # sus4
+
+"""
 
 import sys
 import mido
@@ -20,7 +37,7 @@ import time
 import threading
 
 factor = 2 # duration multiplier
-# time between clock messages at 24 PPQN per beat and 100 BPM
+# time between clocks at 24 PPQN per beat and 100 BPM
 interval = 60 / (100 * 24)
 stop_threads = False # should I stay or should I go?
 
