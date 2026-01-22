6e954e49cf96fb39482633bbd8a4d3f542dcef39
Gene Boggs <gene@ology.net>
2026-01-22 11:13:39 -0800

    WIP

--- multi-timbral-drums.py
+++ multi-timbral-drums.py
@@ -11,47 +11,40 @@ def midi_msg(outport, event, note, channel, velocity):
     msg = mido.Message(event, note=note, channel=channel, velocity=velocity)
     outport.send(msg)
 
-def drum_part(port_name):
+def drum_part():
     global patterns, drums, dura, beats, N, velo, primes
     try:
-        with mido.open_output(port_name) as outport:
-            print(f"Opened output port: {outport.name}")
-            print("Drum machine running... Ctrl+C to stop.")
-            try:
-                while True:
-                    p = random.choice(primes)
-                    patterns['hihat'] = r.euclid(p, beats)
-                    drums['snare'] = random_note()
-                    if N % 2 == 0:
-                        patterns['kick'] = r.euclid(2, beats)
-                    else:
-                        patterns['kick'] = [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1]
-                        drums['kick'] = random_note()
-                        drums['hihat'] = random_note()
-                    for step in range(beats):
-                        if patterns['kick'][step]:
-                            midi_msg(outport, 'note_on', drums['kick'], 0, velo())
-                        if patterns['snare'][step]:
-                            midi_msg(outport, 'note_on', drums['snare'], 1, velo())
-                        if patterns['hihat'][step]:
-                            midi_msg(outport, 'note_on', drums['hihat'], 2, velo())
-                        
-                        time.sleep(dura * 0.9) # slightly shorter than step to prevent overlap
+        while True:
+            p = random.choice(primes)
+            patterns['hihat'] = r.euclid(p, beats)
+            drums['snare'] = random_note()
+            if N % 2 == 0:
+                patterns['kick'] = r.euclid(2, beats)
+            else:
+                patterns['kick'] = [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1]
+                drums['kick'] = random_note()
+                drums['hihat'] = random_note()
+            for step in range(beats):
+                if patterns['kick'][step]:
+                    midi_msg(outport, 'note_on', drums['kick'], 0, velo())
+                if patterns['snare'][step]:
+                    midi_msg(outport, 'note_on', drums['snare'], 1, velo())
+                if patterns['hihat'][step]:
+                    midi_msg(outport, 'note_on', drums['hihat'], 2, velo())
+                
+                time.sleep(dura * 0.9) # slightly shorter than step to prevent overlap
 
-                        if patterns['kick'][step]:
-                            midi_msg(outport, 'note_off', drums['kick'], 0, 0)
-                        if patterns['snare'][step]:
-                            midi_msg(outport, 'note_off', drums['snare'], 1, 0)
-                        if patterns['hihat'][step]:
-                            midi_msg(outport, 'note_off', drums['hihat'], 2, 0)
+                if patterns['kick'][step]:
+                    midi_msg(outport, 'note_off', drums['kick'], 0, 0)
+                if patterns['snare'][step]:
+                    midi_msg(outport, 'note_off', drums['snare'], 1, 0)
+                if patterns['hihat'][step]:
+                    midi_msg(outport, 'note_off', drums['hihat'], 2, 0)
 
-                        time.sleep(dura * 0.1) # Remainder of the step duration
-                    N += 1
-            except KeyboardInterrupt:
-                print("\nDrum machine stopped.")
-    except mido.PortUnavailableError as e:
-        print(f"Error: {e}")
-        print("Check your virtual MIDI port setup and names")
+                time.sleep(dura * 0.1) # Remainder of the step duration
+            N += 1
+    except KeyboardInterrupt:
+        print("\nDrum machine stopped.")
 
 if __name__ == "__main__":
     bpm = 70
@@ -80,7 +73,9 @@ if __name__ == "__main__":
     primes = all_primes(beats, 'list')
 
     try:
-        drum_part('MIDIThing2')
-    except IndexError:
-        print("No MIDI output ports found.")
-        sys.exit(1)
+        with mido.open_output('MIDIThing2') as outport:
+            print(f"Opened output port: {outport.name}")
+            print("Drum machine running... Ctrl+C to stop.")
+            drum_part()
+    except mido.PortUnavailableError as e:
+        print(f"Error: {e}")
