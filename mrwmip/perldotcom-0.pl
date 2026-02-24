aaf3f26a4daf930a711d56583ec7f1e454fdd8ff
Gene Boggs <gene@ology.net>
2026-02-24 11:08:13 -0800

    Fresh

---
+++ mrwmip/perldotcom-0.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use MIDI::RtMidi::FFI::Device;
+
+my $midi_in = RtMidiIn->new;
+my $midi_out = RtMidiOut->new;
+
+print "Input devices:\n";
+$midi_in->print_ports;
+print "\n";
+
+print "Output devices:\n";
+$midi_out->print_ports;
+print "\n";
