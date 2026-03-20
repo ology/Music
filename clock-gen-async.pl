f2d21f05e047a660f7bf4ef6ed28af1f824c9abb
Gene Boggs <gene@ology.net>
2026-03-20 11:11:48 -0700

    Clock

---
+++ clock-gen-async.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+use v5.36;
+use IO::Async::Loop;
+use IO::Async::Timer::Periodic;
+use MIDI::RtMidi::FFI::Device ();
+use Time::HiRes qw(sleep);
+
+my $name = shift || 'SE-02'; # MIDI sequencer device
+my $bpm  = shift || 120;
+
+my $interval = 60 / $bpm / 24;
+
+my $midi_out = RtMidiOut->new;
+$midi_out->open_virtual_port('RtMidiOut');
+$midi_out->open_port_by_name(qr/\Q$name/i);
+
+$SIG{INT} = sub { 
+    say "\nStop";
+    $midi_out->stop;
+    exit;
+};
+
+my $loop = IO::Async::Loop->new;
+my $timer = IO::Async::Timer::Periodic->new(
+   interval => $interval,
+   on_tick => sub {
+        $midi_out->clock;
+   },
+);
+
+$midi_out->start;
+
+$timer->start;
+$loop->add($timer);
+$loop->run;
