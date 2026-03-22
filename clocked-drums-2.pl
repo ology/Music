cc70584799f8a8f0862f7a9c35531b0e49ed1249
Gene Boggs <gene@ology.net>
2026-03-22 12:23:15 -0700

    Drums

--- clocked-drums-2.pl
+++ clocked-drums-2.pl
@@ -21,12 +21,12 @@ my $drums = {
     cymbals => { num => 49, chan => 3 },
 };
 
-my $beats = 16;
 my $clocks_per_beat = 24;
 my $clock_interval = 60 / $bpm / $clocks_per_beat; # seconds / bpm / ppqn
+my $beats = 16;
 my $beat_interval = 60 / $bpm / 4; # 16th-note resolution
-my $ticks = 0;
 my @primes = primes($beats);
+my $ticks = 0;
 
 my $midi_out = RtMidiOut->new;
 $midi_out->open_virtual_port('RtMidiOut');
