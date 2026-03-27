13889bbc3f34bbfa7ac78f687b51a8d1c53947a8
Gene Boggs <gene@ology.net>
2026-03-26 22:19:50 -0700

    Drums

--- clocked-euclidean-drums.pl
+++ clocked-euclidean-drums.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# Play and clock a MIDI drum machine in 4/4.
+# Play and clock a MIDI drum machine in 16-beat sequences.
 # Examples:
 #   perl clocked-euclidean-drums.pl fluid 120 # with fluidsynth
 #   perl clocked-euclidean-drums.pl usb 90 -1 # multi-timbral
