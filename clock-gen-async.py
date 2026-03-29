cd038b6d1a6bdb5b837d8ddcc1f7bf9c0578d7ec
Gene Boggs <gene.boggs@gmail.com>
2026-03-29 10:54:55 -0700

    Fritter

--- clock-gen-async.py
+++ clock-gen-async.py
@@ -1,34 +1,40 @@
 import asyncio
-import mido
-from mido import Message
 import sys
 
-def periodic_task(loop, interval, task_func):
-    task_func()
-    # Schedule the next call recursively
-    loop.call_later(interval, periodic_task, loop, interval, task_func)
+from fritter.boundaries import PhysicalScheduler, SomeScheduledCall
+from fritter.drivers.asyncio import AsyncioTimeDriver
+from fritter.repeat import repeatedly
+from fritter.repeat.rules.seconds import EverySecond
+from fritter.scheduler import schedulerFromDriver
+from mido import Message, open_output  # type:ignore[import-untyped]
 
-def clock():
-    global midi_out
-    midi_out.send(Message('clock'))
+def clock(steps: int, scheduled: SomeScheduledCall) -> None:
+    midi_out.send(Message("clock"))
     print("MIDI clock sent")
 
-async def main(interval):
+
+async def main(interval: float) -> None:
+
     loop = asyncio.get_running_loop()
+
+    driver = AsyncioTimeDriver(loop)
+    scheduler: PhysicalScheduler = schedulerFromDriver(driver)
+    repeatedly(scheduler, clock, EverySecond(interval))
     # delay, callback, *args passed to the callback
-    loop.call_later(interval, periodic_task, loop, interval, clock)
     await asyncio.sleep(10)  # Keep main alive to see it work
 
-if __name__ == '__main__':
-    name = sys.argv[1] if len(sys.argv) > 1 else 'USB MIDI Interface'
-    bpm  = int(sys.argv[2]) if len(sys.argv) > 2 else 120
+
+if __name__ == "__main__":
+    name = sys.argv[1] if len(sys.argv) > 1 else "USB MIDI Interface"
+    bpm = int(sys.argv[2]) if len(sys.argv) > 2 else 120
 
     clock_interval = 60 / bpm / 24
+    # print(clock_interval)
 
-    with mido.open_output(name) as midi_out:
-        midi_out.send(Message('start'))
+    with open_output(name) as midi_out:
+        midi_out.send(Message("start"))
         try:
             asyncio.run(main(clock_interval))
         except KeyboardInterrupt:
-            midi_out.send(Message('stop'))
+            midi_out.send(Message("stop"))
             print("\nStop")
