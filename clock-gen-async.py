eae3e39ccb0cb4328488d0dbfda31a02a7979e13
Gene Boggs <gene.boggs@gmail.com>
2026-03-29 14:19:36 -0700

    Fritter

--- clock-gen-async.py
+++ clock-gen-async.py
@@ -12,7 +12,6 @@ def clock(steps: int, scheduled: SomeScheduledCall) -> None:
     midi_out.send(Message("clock"))
     print("MIDI clock sent")
 
-
 async def main(interval: float) -> None:
 
     loop = asyncio.get_running_loop()
@@ -23,13 +22,11 @@ async def main(interval: float) -> None:
     # delay, callback, *args passed to the callback
     await asyncio.sleep(10)  # Keep main alive to see it work
 
-
 if __name__ == "__main__":
     name = sys.argv[1] if len(sys.argv) > 1 else "USB MIDI Interface"
     bpm = int(sys.argv[2]) if len(sys.argv) > 2 else 120
 
     clock_interval = 60 / bpm / 24
-    # print(clock_interval)
 
     with open_output(name) as midi_out:
         midi_out.send(Message("start"))
