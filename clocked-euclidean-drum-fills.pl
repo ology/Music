ae3eabc6032d2afcda4fe4cc31a9144351c97ff6
Gene Boggs <gene.boggs@gmail.com>
2026-04-01 09:53:10 -0700

    Drums

--- clocked-euclidean-drum-fills.pl
+++ clocked-euclidean-drum-fills.pl
@@ -142,7 +142,7 @@ sub adjust_drums($mcr, $drums, $primes, $toggle, $fill_flag, $filled) {
             my %pats = part_A($mcr, $drums, $primes, $beats);
             my $div = $beats / $size;
             $drums->{hihat}{pat} = [ $pats{hihat}->@[0 .. $div - 1], (0) x $div ];
-            $drums->{kick}{pat}  = [ $pats{kick}->@[0 .. $div - 1], (0) x $div ];
+            $drums->{kick}{pat}  = [ $pats{kick}->@[0 .. $div - 1],  (0) x $div ];
             $drums->{snare}{pat} = [ $pats{snare}->@[0 .. $div - 1], @converted[0 .. $div - 1] ]
             # say ddc $drums;
         }
