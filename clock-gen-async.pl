b4e24b61cbfc03c604cc400c16c2c018a69dff60
Gene Boggs <gene.boggs@gmail.com>
2026-04-02 18:15:53 -0700

    Try

--- clock-gen-async.pl
+++ clock-gen-async.pl
@@ -4,6 +4,7 @@
 # Example: perl clock-gen-async.pl usb 90
 
 use v5.36;
+use feature 'try';
 use IO::Async::Loop ();
 use IO::Async::Timer::Periodic ();
 use MIDI::RtMidi::FFI::Device ();
@@ -15,14 +16,23 @@ my $interval = 60 / $bpm / 24; # seconds / bpm / clocks-per-beat
 
 # open the named midi device for output
 my $midi_out = RtMidiOut->new;
-$midi_out->open_virtual_port('RtMidiOut');
+try { # this will die on windows but is needed for mac
+    $midi_out->open_virtual_port('RtMidiOut');
+}
+catch ($e) {}
 $midi_out->open_port_by_name(qr/\Q$name/i);
 
 $midi_out->start; # start the sequencer
 
 $SIG{INT} = sub { # halt gracefully
     say "\nStop";
-    $midi_out->stop; # stop the sequencer
+    try {
+        $midi_out->panic; # make sure all notes are off
+        $midi_out->stop; # stop the sequencer
+    }
+    catch ($e) {
+        warn "Can't halt the MIDI out device: $e\n";
+    }
     exit;
 };
 
