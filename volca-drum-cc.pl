96f4931b960e9f7d64ae7c97c818fee181d90902
Gene Boggs <gene.boggs@gmail.com>
2026-04-12 15:46:33 -0700

    WIP

--- volca-drum-cc.pl
+++ volca-drum-cc.pl
@@ -171,7 +171,7 @@ __DATA__
   <form method="post">
   <span class="pad-left">Channel:</span> <select id="channel">
 % for my $n (0 .. 5) {
-    <option value="<%= $n %>" <%= $n eq $channel ? 'selected' : '' %>><%= $n %></option>
+    <option value="<%= $n %>" <%= $n eq $channel ? 'selected' : '' %>><%= $n + 1 %></option>
 % }
   </select>
   <p></p>
