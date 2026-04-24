2dffac82f13a48ace0de407c0141738212db21ca
Gene Boggs <gene.boggs@gmail.com>
2026-04-24 11:14:46 -0700

    Lorenz

--- lorenz-attractor.pl
+++ lorenz-attractor.pl
@@ -57,6 +57,7 @@ say $fh 't,x,y,z';
 while ($t <= $t_end) {
     say $fh join ',', map { sprintf '%.8g', $_ } $t, @$y;
     $y = rk4($lorenz, $t, $y, $dt);
+    # XXX naive
     my ($n1) = sprintf '%.0f', uniform_scaling($x_range, $midi_range, $y->[0]);
     my $note = Music::Note->new($n1, 'midinum');
     $n1++ if $note->format('isobase') =~ /[#b]/;
