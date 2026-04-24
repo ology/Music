d7cb1093e10bcec9afe3c5f26c69b4ec9ba0ce46
Gene Boggs <gene.boggs@gmail.com>
2026-04-24 11:10:50 -0700

    Lorenz

--- lorenz-attractor.pl
+++ lorenz-attractor.pl
@@ -3,6 +3,7 @@ use v5.36;
 
 use Math::Utils qw(uniform_scaling);
 use MIDI::Util qw(setup_score);
+use Music::Note ();
 
 my $midi_range = [48, 83];
 my $x_range    = [-25, 25];
@@ -57,8 +58,14 @@ while ($t <= $t_end) {
     say $fh join ',', map { sprintf '%.8g', $_ } $t, @$y;
     $y = rk4($lorenz, $t, $y, $dt);
     my ($n1) = sprintf '%.0f', uniform_scaling($x_range, $midi_range, $y->[0]);
+    my $note = Music::Note->new($n1, 'midinum');
+    $n1++ if $note->format('isobase') =~ /[#b]/;
     my ($n2) = sprintf '%.0f', uniform_scaling($yz_range, $midi_range, $y->[1]);
+    $note = Music::Note->new($n2, 'midinum');
+    $n2++ if $note->format('isobase') =~ /[#b]/;
     my ($n3) = sprintf '%.0f', uniform_scaling($yz_range, $midi_range, $y->[2]);
+    $note = Music::Note->new($n3, 'midinum');
+    $n3++ if $note->format('isobase') =~ /[#b]/;
     # say "N: $n1, $n2, $n3";
     $score->n('qn', $n1, $n2, $n3);
     $t += $dt;
