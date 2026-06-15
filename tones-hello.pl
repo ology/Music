f41abbd7e5b2d2d277aa291c9f70bc6f9ff5cf26
Gene Boggs <gene.boggs@gmail.com>
2026-06-15 12:48:00 -0700

    WIP

--- tones-hello.pl
+++ tones-hello.pl
@@ -4,8 +4,19 @@ use v5.36;
 use feature 'try';
 use Data::Dumper::Compact qw(ddc);
 use MIDI::RtMidi::FFI::Device ();
+use IO::Async::Loop ();
+use IO::Async::Timer::Periodic ();
 
 my $port = shift || 'MIDIThing'; # MIDI device
+my $bpm  = shift || 70; # beats-per-minute
+
+my $beats = 16; # beats in a phrase
+my $divisions = 4; # divisions of a quarter-note into 16ths
+my $clocks_per_beat = 24; # PPQN
+my $clock_interval = 60 / $bpm / $clocks_per_beat; # time / bpm / ppqn
+my $ticks = 0; # clock ticks
+my $beat_count = 0; # how many beats?
+my @queue; # priority queue for note_on/off messages
 
 my @notes = (32, 48, 60);
 
@@ -28,22 +39,36 @@ $SIG{INT} = sub {
     exit;
 };
 
-while (1) {
-    my $note = $notes[int rand @notes];
+my $loop = IO::Async::Loop->new;
 
-    $midi_out->note_on(
-        0,
-        $note,
-        127
-    );
+my $timer = IO::Async::Timer::Periodic->new(
+    interval => $clock_interval,
+    on_tick  => sub {
+        $midi_out->clock;
+        $ticks++;
+        if ($ticks % $clocks_per_beat == 0) {
+            push @queue, $notes[int rand @notes];
+            for my $note (@queue) {
+                $midi_out->note_on(
+                    0,
+                    $note,
+                    127
+                );
+            }
+            $beat_count++;
+        }
+        else {
+            while (my $note = pop @queue) {
+                $midi_out->note_off(
+                    0,
+                    $note,
+                    0
+                );
+            }
+        }
+    },
+);
+$timer->start;
 
-    sleep(1);
-
-    $midi_out->note_off(
-        0,
-        $note,
-        0
-    );
-
-    sleep(1);
-}
+$loop->add($timer);
+$loop->run;
