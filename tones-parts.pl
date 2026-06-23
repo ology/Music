87df38dea02787ca074ef067c5cfa48d2cb9b2ee
Gene Boggs <gene.boggs@gmail.com>
2026-06-23 14:02:18 -0700

    WIP

---
+++ tones-parts.pl
@@ -0,0 +1,163 @@
+#!/usr/bin/env perl
+
+# Play tonal MIDI in real-time!
+# Example(s):
+# perl tones-oo.pl --port=usb --bpm=111 --scales='pentatonic major' --octaves='0 1'
+
+use v5.36;
+use feature 'try';
+use Data::Dumper::Compact qw(ddc);
+use MIDI::RtMidi::FFI::Device ();
+use IO::Async::Loop ();
+use IO::Async::Timer::Periodic ();
+use MIDI::Util qw(dura_size);
+use Getopt::Long qw(GetOptions);
+use Music::VoicePhrase ();
+no warnings 'experimental::try';
+
+my %opt = (
+    port    => 'MIDIThing2',
+    bpm     => 70,
+    base    => 'C',
+    scales  => 'pminor minor',
+    octaves => '0, 1',
+);
+GetOptions(\%opt,
+    'port=s',
+    'bpm=i',
+    'base=s',
+    'scales=s',
+    'octaves=s',
+);
+
+my $beats = 16; # beats in a phrase
+my $divisions = 4; # divisions of a quarter-note into 16ths
+my $clocks_per_beat = 24; # PPQN
+my $clock_interval = 60 / $opt{bpm} / $clocks_per_beat; # time / bpm / ppqn
+my $sixteenth = $clocks_per_beat / $divisions; # clocks per 16th-note
+my $ticks = 0; # clock ticks
+my $beat_count = 0; # how many beats?
+
+my $scales  = [ split /\s+/, $opt{scales} ];
+my $octaves = [ split /\s+/, $opt{octaves} ];
+
+# TODO prompt for part args
+my @parts = (
+    Music::VoicePhrase->new(
+        size      => $divisions,
+        pool      => [qw(dhn hn qn)],
+        weights   => [   1,  2, 2 ],
+        groups    => [   0,  0, 0 ],
+        motif_num => 4,
+        scale     => $scales->[0],
+        octave    => $octaves->[0],
+    ),
+    Music::VoicePhrase->new(
+        size      => $divisions,
+        pool      => [qw(dqn qn en sn)],
+        weights   => [   2,  2, 1, 1 ],
+        groups    => [   0,  0, 2, 2 ],
+        motif_num => 4,
+        scale     => $scales->[1],
+        octave    => $octaves->[0],
+    ),
+);
+
+# open the midi device for output
+my $midi_out = RtMidiOut->new;
+try { $midi_out->open_virtual_port('RtMidiOut') } # needed for mac
+catch ($e) { warn 'Not a Mac' }
+try { $midi_out->open_port_by_name(qr/\Q$opt{port}/i) }
+catch ($e) { die "Can't open MIDI port: $opt{port}\n" }
+say "Sending MIDI to $opt{port} at $opt{bpm} BPM\n";
+
+$midi_out->start; # start the sequencer
+
+# redefine what happens on ^C
+$SIG{INT} = sub { 
+    say "\nStop";
+    try {
+        $midi_out->stop;
+        $midi_out->panic;
+    }
+    catch ($e) {
+        warn "Can't halt the MIDI out device: $e\n";
+    }
+    exit;
+};
+
+my $loop = IO::Async::Loop->new;
+
+my $timer = IO::Async::Timer::Periodic->new(
+    interval => $clock_interval,
+    on_tick  => sub {
+        $midi_out->clock;
+        $ticks++;
+        if ($ticks % $sixteenth == 0) {
+            my $chan = 0;
+
+            if ($beat_count % ($divisions * $divisions) == 0) { # do this every measure:
+                populate($_, $beat_count, $chan++) for @parts;
+            }
+
+            on($_, $beat_count) for @parts;
+
+            $beat_count++;
+        }
+        else {
+            off($_, $beat_count) for @parts;
+        }
+    },
+);
+
+$timer->start;
+$loop->add($timer);
+$loop->run;
+
+sub populate ($m, $count, $chan) {
+    $chan ||= 0;
+    my $motif = $m->motifs->[int rand $m->motifs->@*]; # TODO something clever?
+    say "$count => ", ddc $motif;
+    $m->queue([ map { +{ pitch => $m->voice->rand, duration => $_, chan => $chan } } @$motif ]);
+    say 'Queue: ', ddc $m->queue;
+    # compute the onsets
+    my $tally = 0;
+    my @ons = ($tally);
+    for my $note ($m->queue->@[0 .. $m->queue->@* - 1]) {
+        my $off = dura_size($note->{duration}) * $divisions;
+        $tally += $off;
+        push @ons, $tally;
+        $note->{off} = $count + $off;
+    }
+    $m->onsets([ map { $count + $_ } @ons ]);
+    say 'Onset: ', ddc $m->onsets;
+    $m->index(0); # reset the queue index
+};
+
+sub on ($m, $count) {
+    my $n;
+    # if we are on a beat onset, note_on!
+    if (defined $m->onsets->[$m->index] && $m->onsets->[$m->index] == $count) {
+        $n = $m->queue->[$m->index];
+        say $m->index, ', ', "$count, ", ddc $n;
+        $midi_out->note_on(
+            $n->{chan},
+            $n->{pitch},
+            127 # velocity
+        );
+        $m->increment_index;
+    }
+}
+
+sub off ($p, $count) {
+    for my $n ($p->queue->@*) {
+        if ($beat_count == $n->{off}) {
+            # say 'OFF: ', ddc $n;
+            $midi_out->note_off(
+                $n->{chan},
+                $n->{pitch},
+                0
+            );
+        }
+    }
+}
