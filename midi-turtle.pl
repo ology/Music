cbc2b6d2696075a0bb637310708b16514100ad3b
Gene Boggs <gene.boggs@gmail.com>
2026-08-30 18:17:40 -0700

    Modularize

--- midi-turtle.pl
+++ midi-turtle.pl
@@ -9,22 +9,27 @@ use Music::ScaleNote;
 my $turtle = Data::Turtle->new;
 my $score  = setup_score(bpm => 120);
 my $msn    = Music::ScaleNote->new(scale_note => 'C', scale_name => 'major');
+my $note   = Music::Note->new('C4', 'ISO');
 
-my $note = Music::Note->new('C4', 'ISO');
-
-for (1 .. 8) {
-    my @line = $turtle->forward(10);
-    if ($turtle->pen_status) {
-        my $dura = $line[3] > $line[1] ? 'qn' : 'en'; # crude duration pick
-        $note = $msn->get_offset(
-            note_name   => $note->format('ISO'),
-            note_format => 'ISO',
-            offset      => 1,
-        );
-        $score->n($dura, midi_format($note->format('ISO')));
-    }
-
-    $turtle->right(45);
-}
+phrase($turtle, $score, $msn, $note, 'right');
+phrase($turtle, $score, $msn, $note, 'left');
+phrase($turtle, $score, $msn, $note, 'right');
+phrase($turtle, $score, $msn, $note, 'left');
 
 $score->write_score($0 . '.mid');
+
+sub phrase ($turtle, $score, $msn, $note, $direction) {
+    for (1 .. 16) {
+        my @line = $turtle->forward(10);
+        if ($turtle->pen_status) {
+            my $dura = $line[3] > $line[1] ? 'qn' : 'en'; # crude duration pick
+            $note = $msn->get_offset(
+                note_name   => $note->format('ISO'),
+                note_format => 'ISO',
+                offset      => rand > 0.5 ? 1 : -1,
+            );
+            $score->n($dura, midi_format($note->format('ISO')));
+        }
+        $turtle->$direction(45);
+    }
+}
