8b7a97cd337c5ee9540bed0e98b4f1e7005b723f
Gene Boggs <gene.boggs@gmail.com>
2026-09-03 09:42:17 -0700

    Comment

--- arping.pl
+++ arping.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 # Ex:
+# perl arping.pl # use defaults
 # perl arping.pl --bpm=60 --seq_port=pad --clk_port=usb --arp_type=updown --note_num=4
 # perl arping.pl --b=80 --s=mate --c=usb --a=converge --n=5 --i=46 --d=1 --o=2
 
