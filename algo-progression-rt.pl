cdb3fa72d422a4608ea0552525384028187cfafd
Gene Boggs <gene.boggs@gmail.com>
2026-09-11 10:32:30 -0700

    Comment

--- algo-progression-rt.pl
+++ algo-progression-rt.pl
@@ -20,8 +20,8 @@ Example:
   --parts='DMv-AMv-Bmc-GMc'
 
 While running, press 'p' to pause/resume without closing the MIDI
-port, 'r' to restart the progression from the beginning, press 'n'
-for a new progression, or 'q' to quit.
+port, 'r' to restart the progression from the beginning, 'n' for a
+# new progression, or 'q' to quit.
 
 =head1 CAVEATS
 
