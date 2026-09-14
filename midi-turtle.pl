e338aa9bd082e9214ab1cddf254640a87ce61e2f
Gene Boggs <gene.boggs@gmail.com>
2026-09-14 11:27:04 -0700

    Percent

--- midi-turtle.pl
+++ midi-turtle.pl
@@ -47,6 +47,6 @@ sub phrase ($turtle, $score, $msn, $note, $direction) {
             $score->r($dura);
         }
         $turtle->$direction(45);
-        $turtle->pen_status(rand > 0.2 ? 1 : 0); # rest = 20%
+        $turtle->pen_status(rand > 0.1 ? 1 : 0); # rest = 10%
     }
 }
