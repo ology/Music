6a901374ba6dfe42fdde042242e37ec52bc5d2e6
Gene Boggs <gene.boggs@gmail.com>
2026-09-14 11:25:28 -0700

    Oops

--- midi-turtle.pl
+++ midi-turtle.pl
@@ -39,8 +39,8 @@ sub phrase ($turtle, $score, $msn, $note, $direction) {
                     : (rand > 0.5 ? 1 : -1),
             );
             my $n = midi_format($note->format('ISO'));
-            say "Note: $dura, $n";
-            $score->n($dura, $n) if $verbose;
+            say "Note: $dura, $n" if $verbose;
+            $score->n($dura, $n);
         }
         else {
             say "Rest: $dura" if $verbose;
