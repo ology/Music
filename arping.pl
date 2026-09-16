da505038ec4510747ab618976f1092f32151a632
Gene Boggs <gene.boggs@gmail.com>
2026-09-16 13:14:17 -0700

    Octaves

--- arping.pl
+++ arping.pl
@@ -42,7 +42,7 @@ my %opt = (
     repeats  => 1,       # number of arp-phrase repeats
     initial  => 1,       # number within the 0-based patch indices
     duration => 1,       # 0.1 .. 4 float
-    octave   => '1,2,3', # octaves (0 .. 9 ints)
+    octave   => '3,4,5', # octaves (0 .. 9 ints)
     scale    => 'minor', # scale name as known to Music::Scales
     tonic    => 'C',     # scale key base note
     patches  => undef,   # undef=0..127 or CSV-string of patch numbers
